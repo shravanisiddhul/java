56cdab3cceab0eabc01ecd51b4c5eb92d743e95f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-10 22:07:17 +0530

    Changes to be committed: 	new file:   Output.java

---
+++ Output.java
@@ -0,0 +1,9 @@
+class Output {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.print("Hello Everyone!!\n");
+        System.out.println("Hello World!\nHow are u??");
+    }
+
+    
+}
