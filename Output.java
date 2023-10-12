f92c1b00dea1a9ba73d3829421cac91a44b5aafc
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-12 19:56:58 +0530

    Changes to be committed: 	modified:   Output.java

--- Output.java
+++ Output.java
@@ -1,9 +1,11 @@
-class Output {
+public class Output{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.print("Hello Everyone!!\n");
         System.out.println("Hello World!\nHow are u??");
     }
-
     
 }
+ 
+    
+
