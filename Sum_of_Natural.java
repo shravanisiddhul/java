fd7b4347a5ce2d4a2659e42f5e592a906f6ea760
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-16 08:23:29 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Sum_of_Natural.java

---
+++ Sum_of_Natural.java
@@ -0,0 +1,17 @@
+public class Sum_of_Natural  
+    {  
+    public static void main(String[] args)   
+    {  
+    int i, num = 20, sum = 0;  
+    //executes until the condition returns true  
+    for(i = 1; i <= num; ++i)  
+    {  
+    //adding the value of i into sum variable  
+    sum = sum + i;  
+    }  
+    //prints the sum   
+    System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+    }  
+    }  
+    
+
