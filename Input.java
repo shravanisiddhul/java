e573f152b2ce7b881380b5f32dacb96daf6b3fd7
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-19 20:50:06 +0530

    Changes to be committed: 	new file:   Input.java 	new file:   Main2.java

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Input {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Enter Name : ");
+        String name = sc.next();
+        System.out.println("The Name is : "+ name);
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main2 {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //String name = sc.next();
+        System.out.println("Enter Name , Age , Emp_id : ");
+        //System.out.println("Enter Age = ");
+        //System.out.println("Enter Emp_id = ");
+
+        String name = sc.next();
+        String age = sc.next();
+        String emp_id = sc.next();
+        System.out.println("The Name is = "+ name);
+        System.out.println("The Age is = "+ age);
+        System.out.println("The Emp_id is = "+ emp_id);
+    }
+    
+}
