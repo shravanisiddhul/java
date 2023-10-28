eced3ea637ea8715eb2a0f578ac0f5bd77cbea85
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-28 19:42:08 +0530

    Changes to be committed: 	modified:   .vscode/launch.json     new file:   Do_while.java

---
+++ Do_while.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Do_while {
+    public static void main(String[] args){
+        int i = 1;
+        do{
+            System.out.println(i+" : Hello World!!");
+            i++;
+        }
+        while(i<11);
+    }
+    
+}
