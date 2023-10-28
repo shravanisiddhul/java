95aaa9cf81f9da0fe3cd91815fdef18fd5f30252
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-28 19:14:37 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   For_loop.java 	modified:   Month.java

---
+++ For_loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class For_loop 
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 10; i = i+1)
+        {
+            System.out.println("Hello World");
+        }
+    }
+
+}
