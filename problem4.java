67148f020e5873d2e3da698aea2f5be1b9d441a3
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-28 21:00:19 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   problem4.java

---
+++ problem4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class problem4 {
+    public static void main(String[] args){
+        for( ; ;){
+            System.out.println("Hello Shravani!!");
+        }
+    }
+}
