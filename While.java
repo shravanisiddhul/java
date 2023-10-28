6f1335baeb9e5ae692ecb7737c627cd4b45e7cb5
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-28 19:32:36 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   While.java

---
+++ While.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args){
+        int i = 1;
+        while(i<11){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+    
+}
