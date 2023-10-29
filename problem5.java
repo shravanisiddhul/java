71b965afe3252a89cdb077d1a461b0771dcfabcf
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-29 20:56:00 +0530

    Changes to be committed:  	modified:   .vscode/launch.json 	new file:   GFG.java 	new file:   problem5.java

---
+++ problem5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class problem5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your choice ");
+        
+        System.out.println("Enter either 0 or 1 : ");
+        int choice = sc.nextInt();
+        //int n = sc.nextInt();
+        //int choice = -1;
+        
+        }
+    }
+
