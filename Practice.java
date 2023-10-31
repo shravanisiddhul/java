1beb1c38c0b6f7ab568f7f62c5c2f8a5066557d5
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-10-31 12:47:16 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   Practice.java 	new file:   problem7.java

--- Practice.java
+++ Practice.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
-public class Practice {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no of rows :");
-        int n = sc.nextInt();
-        //System.out.println("Enter no of columns : ");
-        //int m = sc.nextInt();
 
-        for(int i=1; i<=n; i++)
+public class Practice
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows :");
+        int n = sc.nextInt();
+        
+        for(int i=n; i>=1; i--)
         {
-            for(int j=1; j<=i; j++){
-                System.out.print(j);
+            for(int j=1; j<=i; j++)
+            {
+                if((i+j) % 2 == 0 ){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+               
             }
-            
             System.out.println();
         }
     }
