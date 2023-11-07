0019c841f1dcfa63f5440de5fe4a8f6c8e93d34d
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-07 14:23:38 +0530

    Changes to be committed: 	new file:   Pattern10.java 	new file:   Pattern11.java 	new file:   Pattern12.java     new file:   Pattern13.java 	new file:   Pattern14.java 	new file:   Problem10.java

---
+++ Pattern11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Pattern11 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows : ");
+        int n = sc.nextInt();
+        System.out.println("Enter no. of columns : ");
+        int m = sc.nextInt();
+
+        for(int i=n; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=m; j++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
