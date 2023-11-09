1230f951e113eab8b314374c2d6a6d2695e41670
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-09 19:32:50 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Exercise6.java

---
+++ Exercise6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exercise6 {
+   
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=1;
+        do{
+            System.out.println("Hello World!!!");
+        }while(i < n);
+    }    
+}
