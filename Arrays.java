6a7c98b2ab1ba9ba7ebc7f35bb825dab40a6134e
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-14 21:02:44 +0530

    Changes to be committed: 	modified:   Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
-public class Arrays {
-    public static void main(String args [])
+public class Arrays
+{
+    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Size of an array : " );
+        System.out.println("Enter size of an Array : ");
         int size = sc.nextInt();
-        int numbers[] = new int [size];
+        int number[] = new int [size];
 
-        for(int i=0; i<size; i++)
+        for (int i=0; i<size ; i++)
         {
-            numbers[i] = sc.nextInt();
+            number[i]= sc.nextInt();
         }
 
-        for(int i=0;i<numbers.length ; i++)
+        for(int i=0;i<size;i++)
         {
-           System.out.print(numbers[i]);
+            System.out.println(number[i]+" ");
         }
-    }    
+        System.out.println("Enter an Element to be Searched :");
+        int x = sc.nextInt();
+
+        for(int i=0; i<number.length; i++)
+        {
+            if(number[i]==x)
+            {
+                System.out.println("X is found at "+i);
+            }
+        }
+    }
 }
