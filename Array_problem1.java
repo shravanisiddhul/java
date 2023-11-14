fb1a5f40498b4b27d158d0eb3f10b06bb94f2a58
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-14 21:20:40 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Array2.java 	new file:   Array_problem1.java

---
+++ Array_problem1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Array_problem1 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of an Array : ");
+        int size = sc.nextInt();
+
+        String ch[] = new String[size];
+
+        for(int i=0; i<size; i++)
+        {
+            ch[i] = sc.next();
+        }
+
+        for(int i=0; i<size; i++)
+        {
+            System.out.println(ch[i]+" ");
+        }
+    }    
+}
