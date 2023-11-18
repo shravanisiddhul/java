46fde8cd329a259bd4cd75337ce883c15598f8fb
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-18 19:31:53 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   String_concate.java

---
+++ String_concate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class String_concate {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First name : ");
+        String firstname = sc.next();
+        System.out.println("Enter Last Name : ");
+        String lastname = sc.next();
+
+        String fullname = firstname +" "+ lastname;
+        System.out.println("Fullname is : "+fullname);
+    }
+}
