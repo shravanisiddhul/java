bef2390b190eb4428c93842ac760e7666c0f5e4e
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-18 20:40:40 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   String_problem1.java

---
+++ String_problem1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class String_problem1 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int size = sc.nextInt(); 
+
+        String array[] = new String[size];
+
+        System.out.println("Enter Strings in Array : ");
+
+        for(int i=0; i<size;i++)
+        {
+            array[i] = sc.next();
+        }
+
+        int TotalLength = 0;
+        for(int i=0; i<array.length;i++)
+        {
+            System.out.println(array[i]);
+            TotalLength = array[i].length();
+        }
+        System.out.println(TotalLength);
+
+    }    
+}
