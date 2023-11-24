167fd221d7c860417118f2a54a3b9e55e67b9d56
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-24 20:24:40 +0530

    Changes to be committed: 	modified:   String_Builder.java

--- String_Builder.java
+++ String_Builder.java
@@ -9,8 +9,18 @@ public class String_Builder {
         System.out.println(sb.charAt(4));
 
        //character change
-       sb.setCharAt(1, 't');
-       System.out.println(sb);
-    
+        sb.setCharAt(1, 't');
+        System.out.println(sb);
+
+        sb.setCharAt(1, 'h');
+        System.out.println(sb);
+
+       //insert character
+        sb.insert(5, 'a');
+        System.out.println(sb);
+
+        //delete 
+        sb.delete(5, sb.length());
+        System.out.println(sb);
 }
 }
