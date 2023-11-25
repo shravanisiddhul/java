8a0b9710655436b4c190102feb65bf05199caa55
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-25 21:39:05 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   String_B1.java

---
+++ String_B1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class String_B1 {
+    public static void main(String args [])
+    {
+     StringBuilder sb = new StringBuilder("tony");
+     //Insert char
+     sb.insert(0, 'S');
+     System.out.println(sb);
+   }
+}
+
+
