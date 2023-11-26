4fae6abc020b6ef5bfc1ac5d35905d15ff1a1dfa
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-26 11:57:50 +0530

    Here i started practicing all question from start to until now
     Changes to be committed:
    	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,23 +4,6 @@ public class Practice
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no. of rows :");
-        int n = sc.nextInt();
-        
-        for(int i=n; i>=1; i--)
-        {
-            for(int j=1; j<=i; j++)
-            {
-                if((i+j) % 2 == 0 ){
-                    System.out.print("1 ");
-                }
-                else{
-                    System.out.print("0 ");
-                }
-               
-            }
-            System.out.println();
-        }
+        System.out.println("Hello World");
     }
 }
