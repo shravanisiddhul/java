2c1c6276fb8e8aead7e9b21e10745d560d006c6a
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-26 12:37:11 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -5,18 +5,12 @@ public class Practice
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number : ");
-        int n = sc.nextInt();
-        int fact = 1;
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine();
 
-        for(int i=1;i<=n; i++)
+        for(int i=str.length();i>0; i--)
         {
-            fact = fact * i ;
+            System.out.print(str.charAt(i-1));
         }
-        System.out.println("Factorial is : "+fact);
-        
-
-
-
     }
 }
