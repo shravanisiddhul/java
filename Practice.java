350f9b97ea9d84c6dfea34dacec9311da52fcd3f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-27 20:18:57 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,20 +1,18 @@
 import java.util.*;
 public class Practice
 {
-    
-        static int factorial(int n){    
-         if (n == 0)    
-           return 1;    
-         else    
-           return(n * factorial(n-1));    
-        }    
+    public static void main(String args [])
+    {
+        int a = 0101;
+        int b = 0110;
 
-        public static void main(String args[])
-        {  
-         int i,fact=1;  
-         int number=4;//It is the number to calculate factorial    
-         fact = factorial(number);   
-         System.out.println("Factorial of "+number+" is: "+fact);    
-        }  
-        
+        int c =(a & b);
+        int d =(a | b);
+        int e =(a ^ b);
+        int f =~a;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }        
 }
