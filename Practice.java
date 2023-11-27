f8370ee7768a7a5bb418cdd7aff5881d076e1629
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-27 09:37:35 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,21 +4,29 @@ public class Practice
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number : ");
-        int n = sc.nextInt();
+        System.out.println("Enter Size : ");
+        int size = sc.nextInt();
+        int sum = 0;
         
-        int f1=0;
-        int f2=1;
-        int f3;
-        System.out.println("Fibonacci series is : ");
-        System.out.print(f1+" "+f2);
-        for(int i=2; i<=n; i++)
-        {
-            f3 = f1 + f2;
-            System.out.print(" "+f3);
-            f1 = f2;
-            f2 = f3;
+        int number[] = new int [size];
 
+        System.out.println("Enter Array elements : ");
+        for(int i=0; i<size; i++)
+        {
+            number[i] = sc.nextInt();
         }
+
+        System.out.println("Entered Array Elements are : ");
+        System.out.print("{");
+        for(int i= 0; i<number.length; i++)
+        {
+            System.out.print(number[i]+" ");
+            sum = sum + number[i];
+            
+        }
+        System.out.println("}");
+        System.out.println();
+        System.out.println("Sum is "+sum);
+        
 } 
 }
