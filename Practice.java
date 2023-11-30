bcd9c9c4e0a5dd12bff1ddd2b4669c741d2b59f1
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-30 17:26:25 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -12,6 +12,7 @@ public class Practice
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array : ");
         int arr[] = new int[5];
 
         for(int i=0; i<arr.length; i++)
@@ -19,20 +20,30 @@ public class Practice
             arr[i] = sc.nextInt();
         }
 
-        for(int i=0; i<arr.length-1; i++)
+        for(int i=1; i<arr.length; i++)
         {
-            int smallest = i;
-            for(int j=i+1; j<arr.length; j++)
+            int current = arr[i];
+            int j = i-1;
+            while(j >= 0 && current < arr[j])
             {
-                if(arr[smallest] > arr[j])
-                {
-                    smallest = j;
-                }
-                int temp = arr[smallest];
-                arr[smallest] = arr[i];
-                arr[i] = temp;
+                arr[j+1] = arr[j];
+                j--;
             }
+            arr[j+1] = current;
         }
         Array(arr);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
