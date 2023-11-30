b84194d5cf9a48ca2756b325faaa2f45deb12466
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-30 17:11:54 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class InsertionSort  {
+    public static void Array(int arr[])
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array : ");
+        int arr[] = new int [5];
+
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        for(int i=1; i<arr.length; i++)
+        {
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j])
+            {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        Array(arr);
+    }
+}
