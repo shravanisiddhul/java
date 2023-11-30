aa63678f85983442a89615080456976da15550de
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-11-30 16:30:06 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -21,14 +21,16 @@ public class Practice
 
         for(int i=0; i<arr.length-1; i++)
         {
-            for(int j=0; j<arr.length-i-1; j++)
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++)
             {
-                if(arr[j] > arr[j+1])
+                if(arr[smallest] > arr[j])
                 {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    smallest = j;
                 }
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
             }
         }
         Array(arr);
