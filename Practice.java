9eb8530a097af53265b0c28c946e866c98461372
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-12-01 12:07:29 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,36 +1,32 @@
 import java.util.*;
-public class Practice {
+public class Practice{
     public static void Array(int arr[])
     {
-        for(int i=0; i<arr.length;i++)
-        {
-            System.out.print(arr[i]+" ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
         }
         System.out.println();
     }
     public static void main(String args [])
     {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter An Array : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter An Array: ");
         int arr[] = new int [5];
 
-        for(int i=0; i<arr.length; i++)
-        {
-            arr[i] =sc.nextInt();
+        for(int i=0 ; i<arr.length; i++){
+            arr[i] = sc.nextInt();
         }
 
-        for(int i=0; i<arr.length-1; i++)
+        for(int i=1; i<arr.length; i++)
         {
-            for(int j=0; j<arr.length-i-1; j++)
+            int current =arr[i];
+            int j = i-1;
+            while(j >= 0 && current < arr[j])
             {
-                if(arr[j] > arr[j+1])
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-
-                }
+                arr[j+1] = arr[j];
+                j--;
             }
+            arr[j+1] = current;
         }
         Array(arr);
     }
