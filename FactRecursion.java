16ce79e8d67caa9d76bb782b7e3670750822e485
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2023-12-01 13:33:17 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   FactRecursion.java

---
+++ FactRecursion.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FactRecursion {
+    public static void PrintFact(int i,int n,int fact)
+    {
+        if(i == n+1)
+        {
+            System.out.println(fact);
+            return;
+        }
+
+        fact = fact * i ;
+        PrintFact(i+1, n, fact);
+    }
+    public static void main(String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int n = sc.nextInt();
+        int i = 1;
+        int fact = 1;
+        PrintFact(i, n, fact);
+
+    }    
+}
