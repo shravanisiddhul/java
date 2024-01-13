a611b7c7e01a1acfa73a35ef389c33e8bc1ec987
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-13 19:20:43 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,28 +1,12 @@
 import java.util.*;
 public class Practice
 {
-    public static void main(String args[])
+    public static void main(String args [])
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of an Array : ");
-        int size = sc.nextInt();
+        char ch = 'x';
+        int Ascii = ch;
 
-        int Num[] = new int [size];
-
-        System.out.println("Enter Elements in Array : ");
-        for(int i = 0; i<size; i++)
-        {
-            Num[i] = sc.nextInt();
-        }
-        
-        System.out.println("Enter Element to be Serached : ");
-        int x = sc.nextInt();
-        for(int i=0; i<Num.length; i++)
-        {
-            if(Num[i] == x)
-            {
-                System.out.println("X is found At : "+i);
-            }
-        }       
+        int castAscii = (int) ch;
+        System.out.println("Ascii Value of "+ch+ " is "+castAscii);
     }
 }
