52c1c31fe4b1ed3958c4840a19b4f3442b3c3a3a
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-13 19:34:16 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,12 +1,40 @@
 import java.util.*;
 public class Practice
 {
+    public static void SortedArray(int Arr[])
+    {
+        for(int i=0; i<Arr.length; i++)
+        {
+            System.out.print(Arr[i]+" ");
+        }
+    }
     public static void main(String args [])
     {
-        char ch = 'x';
-        int Ascii = ch;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of an Array : ");
+        int size = sc.nextInt();
 
-        int castAscii = (int) ch;
-        System.out.println("Ascii Value of "+ch+ " is "+castAscii);
+        int Arr[] = new int [size];
+
+        System.out.println("Enter Elements in an Array : ");
+        for(int i=0; i<size; i++)
+        {
+            Arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Sorted Array : ");
+        for(int i=0; i<Arr.length-1; i++)
+        {
+            for(int j=0; j<Arr.length-i-1; i++)
+            {
+                if(Arr[j] > Arr[j+1])
+                {
+                    int temp = Arr[j];
+                    Arr[j] = Arr[j+1];
+                    Arr[j+1] = temp;
+                }
+            }
+        }
+        SortedArray(Arr);
     }
 }
