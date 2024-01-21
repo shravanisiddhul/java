2b96289f5449feb985fcfc585b1c387c7abcdd31
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-21 20:54:56 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,60 +4,17 @@ public class Practice
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number :");
+        System.out.println("Enter a Number of Rows :");
         int n = sc.nextInt();
-
-        boolean isPrime = true;
-        for(int i=2;i<=n/2;i++)
+        System.out.println("Enter no. of columns :");
+        int m = sc.nextInt();
+        for(int i=0; i<n;i++)
         {
-            if(n%i == 0)
+            for(int j=0;j<m;j++)
             {
-                isPrime = false;
-                break;
+                System.out.print("*");
             }
-        }
-        if(isPrime)
-        {
-            if(n==1)
-            {
-                System.out.println("Neither Prime Nor composite");
-            }
-            else{
-                System.out.println("Prime");
-            }
-        }
-        else{
-            System.out.println("Not Prime");
+            System.out.println();
         }
     }
-
 }
-// import java.util.*;
-
-
-// public class Practice {
-//    public static void main(String args[]) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-
-
-//        boolean isPrime = true;
-//        for(int i=2; i<=n/2; i++) {
-//            if(n % i == 0) {
-//                isPrime = false;
-//                break;
-//            }
-//        }
-
-
-//        if(isPrime) {
-//            if(n == 1) {
-//                System.out.println("This is neither prime not composite");
-//            } else {
-//                System.out.println("This is a prime number");
-//            }
-//        } else {
-//            System.out.println("This is not a prime number");
-//        }
-//    }   
-// }
