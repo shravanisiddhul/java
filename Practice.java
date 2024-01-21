979663dc17edd52d51bbde08e15468ca71941919
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-21 20:04:27 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -7,10 +7,58 @@ public class Practice
         System.out.println("Enter a No. : ");
         int n = sc.nextInt();
 
-        System.out.println("Table of "+n+" is :");
-        for(int i=1;i<=10;i++)
+        switch(n)
         {
-            System.out.println(n+" * "+i+" = "+n*i);
+            case 1:
+            System.out.println("January");
+            break;
+
+            case 2:
+            System.out.println("February");
+            break;
+
+            case 3:
+            System.out.println("March");
+            break;
+
+            case 4: 
+            System.out.println("April");
+            break;
+
+            case 5:
+            System.out.println("May");
+            break;
+
+            case 6:
+            System.out.println("June");
+            break;
+
+            case 7: 
+            System.out.println("July");
+            break;
+
+            case 8:
+            System.out.println("August");
+            break;
+
+            case 9:
+            System.out.println("Sept");
+            break;
+
+            case 10:
+            System.out.println("Oct");
+            break;
+
+            case 11:
+            System.out.println("Nov");
+            break;
+
+            case 12:
+            System.out.println("Dec");
+            break;
+
+            default:
+            System.out.println("Wrong input");
         }
     }
 }
