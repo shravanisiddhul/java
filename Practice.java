c99885e640b3425394a0d3e0c4381272d5c79edf
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-21 21:05:11 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -6,20 +6,13 @@ public class Practice
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number of Rows :");
         int n = sc.nextInt();
-        System.out.println("Enter no. of columns :");
-        int m = sc.nextInt();
-        for(int i=0; i<n;i++)
+        // System.out.println("Enter no. of columns :");
+        // int m = sc.nextInt();
+         for(int i=1; i<=n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<=i;j++)
             {
-                if(i==0 || i==n-1 || j==0 || j==m-1)
-                {
                     System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            
             }
             System.out.println();
         }
