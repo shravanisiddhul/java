c4703e7f40113f7ab3dabb0ead0bdbf5d6634310
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-21 20:33:44 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,15 +1,34 @@
 import java.util.*;
 public class Practice
 {
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter a No. : ");
-        int n = sc.nextInt();
+    public static void main(String args [])
 
-        for(int i=0; i<n; i=i+2)
-        {
-            System.out.println(i);
-        }
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 0 or 1:");
+        int n =sc.nextInt();
+
+        
+        do{
+            System.out.println("Enter Marks :");
+            int marks = sc.nextInt();
+            
+            if(marks>=90 && marks<=100)
+            {
+                System.out.println("This is Good");
+            }
+            else if(marks >=89 && marks<=60)
+            {
+                System.out.println("This is also Good");
+            }
+            else 
+            {
+                System.out.println("This is Good as well");
+            }
+            System.out.println("Do you want to continue \nThen Enter 0 or 1 :");
+            n =sc.nextInt();
+
+        }while( n==1);
+    
     }
 }
