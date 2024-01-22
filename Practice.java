03487e8adf34454ee93bfdd8d5cb75418ef6ba7b
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-22 13:48:26 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -6,7 +6,7 @@ public class Practice {
         System.out.println("Enter no.of rows :");
         int n = sc.nextInt();
        
-       for(int i=1;i<=n;i++)
+       for(int i=n;i>=1;i--)
        {
             for(int j=1;j<=i;j++)
             {
