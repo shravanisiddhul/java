164c47c9e0e252f044cb441d3b6fa4e9ae6a0a01
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-22 14:33:40 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -14,8 +14,12 @@ public class Practice {
             {
                 System.out.print(" ");
             }
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++)
+            {
+                System.out.print(j);
             }
            
             System.out.println();
