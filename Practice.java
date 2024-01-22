b001748f23d3bec09d7fd40b39840d2f85c47c15
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-22 13:55:18 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -5,15 +5,18 @@ public class Practice {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no.of rows :");
         int n = sc.nextInt();
-       
-       for(int i=n;i>=1;i--)
-       {
-            for(int j=1;j<=i;j++)
+        int num = 1;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print(num+" ");
+                num++;
             }
             System.out.println();
         }
+       
     }
 }
 
