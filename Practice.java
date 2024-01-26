3eebe23ba6b25aa95d1c6ca5dd5b7e90ce3095ed
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-26 12:01:42 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,18 +1,28 @@
 import java.util.*;
 public class Practice{
-    public static void multiply(int a,int b)
+    public static void Factorial(int n,int fact)
     {
-        int mul = a * b;
-        System.out.println("Multiplication of "+a+" and "+b+ " is : "+mul);
+        
+        if(n<0)
+        {
+            System.out.println("Invalid number ");
+            return;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of "+n+ " is : "+fact);
+        return;
     }
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a :");
-        int a = sc.nextInt();
-        System.out.println("Enter b : ");
-        int b = sc.nextInt();
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
 
-        multiply(a,b);
+        int fact = 1;
+
+        Factorial(n,fact);        
     }
 }
