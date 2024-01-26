dabb0a610c6fefff8d6e932b6687a2123b6ba1f9
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-26 12:18:12 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,26 +1,17 @@
 import java.util.*;
 public class Practice
 {
-    public static void Isprime(int n) 
+    public static void EvenOrNot(int n) 
     {
-        boolean isPrime = true;
-        for(int i=2;i<=n/2;i++)
+        if (n % 2 ==0) 
         {
-            if(n % i == 0)
-            {
-                isPrime=false;
-                break;
-            }
-        }
-        if(isPrime)
-        {
-            System.out.println(n+" is a Prime Number ");
-        }
+            System.out.println("No is Even");
+        } 
         else{
-            System.out.println(n+" is not a Prime Number");
-        }
-
+            System.out.println("No is Odd");
+        }   
     }
+
     
     public static void main(String args[])
     {
@@ -28,6 +19,6 @@ public class Practice
         System.out.println("Enter a Number : ");
         int n = sc.nextInt();
 
-        Isprime(n);
+        EvenOrNot(n);
     }
 }
