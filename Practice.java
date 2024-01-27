c8e4dd7302cb1389f72630ac89ce87164f1bc1d9
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-27 14:10:01 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,24 +1,18 @@
 import java.util.*;
 public class Practice{
-    public static void GreaterNo(int a,int b){
-        if(a>b)
-        {
-            System.out.println(a+" is grater than "+b);
-        }
-        else{
-            System.err.println(b+" is geater than "+a);
-        }
+    public static void Circumference(float r,float pi){
+        float circumference = 2*pi*r;
+        System.out.println("Circumferenceof a Circle :"+circumference );
         return;
     }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 2 Numbers ");
-        System.out.println("Enter A :");
-        int a = sc.nextInt();
-        System.out.println("Enter B : ");
-        int b = sc.nextInt();
+        
+        System.out.println("Enter radius :");
+        float r = sc.nextFloat();
+        float pi = (float) 3.14;
 
-        GreaterNo(a, b);
+        Circumference(r,pi);
     }
 }
