73cad67e45dc206c368ee231d4c93d4787b7d46a
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-27 13:54:05 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,21 +1,22 @@
 import java.util.*;
-public class Practice
-{
-    public static void Table(int n) 
+public class Practice{
+    public static void Average(int a,int b,int c )
     {
-       for(int i=1;i<=10;i++)
-       {
-            System.out.println(n+" * "+i+" = "+n*i);
-       }   
+        float average = (a+b+c)/3;
+        System.out.println("Average of 3 Numbers are :"+average);
+        return;
     }
-
-    
-    public static void main(String args[])
+    public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number : ");
-        int n = sc.nextInt();
+        System.out.println("Enter 3 No :");
+        System.out.println("Enter A :");
+        int a = sc.nextInt();
+        System.out.println("Enter B : ");
+        int b = sc.nextInt();
+        System.out.println("Enter C :");
+        int c = sc.nextInt();
 
-        Table(n);
+        Average(a, b, c);
     }
 }
