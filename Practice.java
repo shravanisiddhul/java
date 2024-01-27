d910ce3006c8b4caa9e11432123ddb52557f7709
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-27 13:58:19 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,22 +1,20 @@
 import java.util.*;
 public class Practice{
-    public static void Average(int a,int b,int c )
+    public static void SumOfOdd(int n,int sum)
     {
-        float average = (a+b+c)/3;
-        System.out.println("Average of 3 Numbers are :"+average);
+        for(int i=1;i<=n;i=i+2)
+        {
+            sum = sum + i;
+        }
+        System.out.println("Sum of all Odd Numbers : "+sum);
         return;
     }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 3 No :");
-        System.out.println("Enter A :");
-        int a = sc.nextInt();
-        System.out.println("Enter B : ");
-        int b = sc.nextInt();
-        System.out.println("Enter C :");
-        int c = sc.nextInt();
-
-        Average(a, b, c);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        int sum = 0;
+        SumOfOdd(n,sum);
     }
 }
