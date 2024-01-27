94f3bb28b1198cadf68d288dff47d2ba23592d4c
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-27 14:15:08 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,18 +1,24 @@
 import java.util.*;
 public class Practice{
-    public static void Circumference(float r,float pi){
-        float circumference = 2*pi*r;
-        System.out.println("Circumferenceof a Circle :"+circumference );
+    public static void EligibilityOfPerson(int age){
+
+        if(age <= 0)
+        {
+            System.out.println("Invalid age");
+        }else if(age<18){
+            System.out.println("Person is not Eligible");
+        }else{
+            System.out.println("Person is Eligible");
+        }
         return;
     }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter radius :");
-        float r = sc.nextFloat();
-        float pi = (float) 3.14;
-
-        Circumference(r,pi);
+        System.out.println("Enter Age :");
+        int  age = sc.nextInt();
+        
+       EligibilityOfPerson(age);
     }
 }
