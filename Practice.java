b43242fb573a46261467afcfc82b2275b3c6fb00
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-27 14:22:06 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,24 +1,13 @@
 import java.util.*;
 public class Practice{
-    public static void EligibilityOfPerson(int age){
-
-        if(age <= 0)
-        {
-            System.out.println("Invalid age");
-        }else if(age<18){
-            System.out.println("Person is not Eligible");
-        }else{
-            System.out.println("Person is Eligible");
-        }
-        return;
-    }
-    public static void main(String args [])
+    
+    public static void Practice1(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter Age :");
-        int  age = sc.nextInt();
-        
-       EligibilityOfPerson(age);
+        int n = sc.nextInt();
+        int i=1;
+        do{
+            System.out.println("Hello World");
+        }while(i<n);
     }
 }
