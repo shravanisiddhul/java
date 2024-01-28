2ba1effc164d34380abb4a5d2f16183cafb732b6
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-28 16:57:24 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,25 +1,21 @@
 import java.util.*;
 public class Practice{
-    public static void Fibo(int n,int n1,int n2)
-    {
-        for(int i=2;i<n;i++)
-        {
-            int n3 = n1 + n2;
-            System.out.print(" "+n3);
-            n1 = n2;
-            n2 = n3;
-        }
-
-    }
+    
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no : ");
-        int n = sc.nextInt();
+        System.out.println("Enter size : ");
+        int size = sc.nextInt();
 
-        int n1 = 0;
-        int n2 = 1;
-        System.out.print(n1+" "+n2);
-        Fibo(n, n1, n2);
+        int arr[] = new int [size];
+
+        for(int i=0;i<size;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(i+" ");
+        }
     }
 }
