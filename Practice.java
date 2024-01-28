ebf27a0fc90bc624c60a75803442850a7f8bfe66
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-28 17:16:15 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -7,15 +7,16 @@ public class Practice{
         System.out.println("Enter size : ");
         int size = sc.nextInt();
 
-        int arr[] = new int [size];
-
+        String  arr[] = new String [size];
+        System.out.println("Enter Names : ");
         for(int i=0;i<size;i++)
         {
-            arr[i] = sc.nextInt();
+            arr[i] = sc.next();
         }
+        System.out.println("Name of "+size+" BTS members  are : ");
         for(int i=0;i<arr.length;i++)
         {
-            System.out.print(i+" ");
+            System.out.println((i+1)+". "+arr[i]);
         }
     }
 }
