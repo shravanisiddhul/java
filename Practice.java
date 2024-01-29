0e4988c480acf0fd95708c02a3f8c339d284a656
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-29 13:53:34 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,30 +1,50 @@
 import java.util.*;
-public class Practice{
-    
+public class Practice
+{
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter rows :");
+        System.out.println("Enter rows : ");
         int rows = sc.nextInt();
-        System.out.println("Enter Columns : ");
-        int col = sc. nextInt();
-        
-        int  arr[][] = new int [rows][col];
-        System.out.println("Enter Numbers : ");
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<col;j++)
+        System.out.println("Enter Columns: ");
+        int cols = sc.nextInt();
+
+        System.out.println("Enter elements in an array : ");
+        int arr[][]= new int [rows][cols];
+
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
             {
-                arr[i][j]= sc.nextInt();
+                arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println("2D Array is : ");
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<col;j++)
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
             {
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
         
+        System.out.println("Enter element to be searched : ");
+        int x = sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(arr[i][j] ==  x)
+                {
+                    System.out.println(x+ " is found at ("+i+","+j+")");
+                    found = true;
+                }
+            }
+        }
+        if(!found)
+        {
+            System.out.println(x+ " is not found in the array ");
+        }
     }
 }
