c1d425647c09d5e89250805f95176e807da1e763
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-29 12:58:56 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,44 +4,26 @@ public class Practice{
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size : ");
-        int size = sc.nextInt();
-
-        int  arr[] = new int [size];
-        System.out.println("Enter Numbers : ");
-        for(int i=0;i<size;i++)
-        {
-            arr[i] = sc.nextInt();
-        }
+        System.out.println("Enter rows :");
+        int rows = sc.nextInt();
+        System.out.println("Enter Columns : ");
+        int col = sc. nextInt();
         
-        boolean isAscending = true;
-        for(int i=0; i<arr.length-1;i++)
-        {
-            if(arr[i] > arr[i+1])
+        int  arr[][] = new int [rows][col];
+        System.out.println("Enter Numbers : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++)
             {
-                isAscending = false;
+                arr[i][j]= sc.nextInt();
             }
         }
-        if(isAscending)
-        {
-            System.out.println("Array is in Ascending order");
-        }else{
-            System.out.println("Array is not in ascending order");
-        }
-
-        System.out.println("Sorted Array is : ");
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=i+1 ;j<arr.length;j++)
+        System.out.println("2D Array is : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++)
             {
-                if(arr[i] > arr[j])
-                {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+                System.out.print(arr[i][j]+" ");
             }
-            System.out.print(arr[i]+ " ");
+            System.out.println();
         }
         
     }
