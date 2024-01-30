a517cbb814817159bfc2b57a06d231185d0a25ce
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-30 19:04:36 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -2,26 +2,31 @@ import java.util.*;
 public class Practice
 {
     // Print Fibonacci Series
-    public static void PrintFibo(int f1,int f2,int n)
+    public static int PrintPower(int x,int n)
     {
         
         if(n == 0)
         {
-            return;
+            
+            return 1;
         }
-        int f3 = f1 + f2;
-        System.out.print(" "+f3);
-        PrintFibo(f2, f3,n-1);
+        if(x == 0)
+        {
+            return 0;
+        }
+        int xpownm1 = PrintPower(x, n-1);
+        int xpown = x * xpownm1;
+        System.out.println(x+ " raised to "+n+ " is : "+xpown);
+        return xpown;
     }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number : ");
+        int x = sc.nextInt();
+        System.out.println("Enter Power No :");
         int n = sc.nextInt();
-        int f1 = 0;
-        int f2 = 1;
-        System.err.print(f1+" "+f2);
 
-        PrintFibo(f1, f2, n-2);
+        PrintPower(x, n);
     }
 }
