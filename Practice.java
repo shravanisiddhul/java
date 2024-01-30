0da6e0e4887b4c209d5a16a6238b1f5a51eb4ef0
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-30 13:57:21 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -17,19 +17,18 @@ public class Practice{
             arr[i] = sc.nextInt();
         }
 
-        // Selection Sort
-        for(int i=0;i<arr.length;i++){
-            int smallest = i;
-            for(int j=i+1;j<arr.length;j++)
+        // Insertion  Sort
+
+        // Unsorted Array
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j])   
             {
-                if(arr[smallest] > arr[j])
-                {
-                    smallest = j;
-                }
-            }
-            int temp =arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
+                arr[j+1] = arr[j];
+                j--;
+            } 
+            arr[j+1] = current;       
         }
         PrintArray(arr);
     }
