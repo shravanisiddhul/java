2d13cf7e2a9ed166f04aa2067f64a4404b68724c
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-30 14:34:30 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,35 +1,18 @@
 import java.util.*;
-public class Practice{
-    public static void PrintArray(int arr[])
+public class Practice
+{
+    public static void Print(int n)
     {
-        System.out.println("Sorted Array : ");
-        for(int i=0;i<arr.length;i++)
+        if(n == 0)
         {
-            System.out.print(arr[i]+ " ");
+            return;
         }
+        System.out.println(n);
+        Print(n-1);
     }
     public static void main(String args [])
     {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int [7];
-        for(int i=0;i<7;i++)
-        {
-            arr[i] = sc.nextInt();
-        }
-
-        // Insertion  Sort
-
-        // Unsorted Array
-        for(int i=1;i<arr.length;i++){
-            int current = arr[i];
-            int j = i-1;
-            while(j>=0 && current < arr[j])   
-            {
-                arr[j+1] = arr[j];
-                j--;
-            } 
-            arr[j+1] = current;       
-        }
-        PrintArray(arr);
+        int n = 5;
+        Print(n);
     }
 }
