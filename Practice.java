a1e2c2d1310ba06a1fe84141f2c274c88821c0ac
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-30 15:27:58 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,26 +1,27 @@
 import java.util.*;
 public class Practice
 {
-    public static void CalFactorial(int i,int n,int fact)
+    // Print Fibonacci Series
+    public static void PrintFibo(int f1,int f2,int n)
     {
         
-        if(i == n)
+        if(n == 0)
         {
-            fact = fact * i;
-            System.out.println("Factorial of "+n+" number is : "+fact);
             return;
         }
-        fact = fact * i;
-        CalFactorial(i+1, n, fact);
+        int f3 = f1 + f2;
+        System.out.print(" "+f3);
+        PrintFibo(f2, f3,n-1);
     }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number : ");
         int n = sc.nextInt();
-        int fact = 1;
-        int i = 1;
+        int f1 = 0;
+        int f2 = 1;
+        System.err.print(f1+" "+f2);
 
-        CalFactorial(i, n, fact);
+        PrintFibo(f1, f2, n-2);
     }
 }
