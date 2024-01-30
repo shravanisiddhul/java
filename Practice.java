aaf9af61e0f67dba264f145642bff10136f3f0cb
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-30 13:12:52 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -3,28 +3,28 @@ public class Practice{
     public static void PrintArray(int arr[])
     {
         System.out.println("Sorted Array : ");
-        for(int i=0;i<arr.length;i++)
+        for(int i=0; i<arr.length; i++)
         {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
     }
-
     public static void main(String args [])
-    {
-        int arr[] = {1,22,3,2,5,5,3};
+    { 
+        int arr[] = {1,5,2,4,7,6};
 
         for(int i=0;i<arr.length-1;i++)
         {
-            for (int j=0;j<arr.length-i-1;j++)
+            int smallest = i;
+            for(int j = i+1;j<arr.length;j++)
             {
-                if(arr[j] > arr[j+1])
-                {
-                    int temp=arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]=temp;
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
                 }
+
             }
-            
+            int temp= arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp ;
         }
         PrintArray(arr);
     }
