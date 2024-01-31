b913e1eca35a3e68a8f750fad5c665d3eefe5c1e
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-01-31 20:46:29 +0530

    Changes to be committed: 	modified:   Practice.class 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,32 +1,29 @@
 import java.util.*;
 public class Practice
 {
-    // Print Fibonacci Series
-    public static int PrintPower(int x,int n)
-    {
-        
-        if(n == 0)
-        {
-            
-            return 1;
-        }
-        if(x == 0)
-        {
-            return 0;
-        }
-        int xpownm1 = PrintPower(x, n-1);
-        int xpown = x * xpownm1;
-        System.out.println(x+ " raised to "+n+ " is : "+xpown);
-        return xpown;
-    }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number : ");
-        int x = sc.nextInt();
-        System.out.println("Enter Power No :");
-        int n = sc.nextInt();
+        System.out.println("Enter your Marks :");
+        int marks = sc.nextInt();
+        
 
-        PrintPower(x, n);
+        if(marks >= 90 && marks <= 100)
+        {
+            System.out.println("A grade");
+        }else if(marks >= 80 && marks <= 89)
+        {
+            System.out.println(" B grade");
+        }else if (marks >=70 && marks <= 79)
+        {
+            System.out.println("C grade");
+        }
+        else if(marks >= 60 && marks <= 69)
+        {
+            System.out.println("D grade");
+        }else
+        {
+            System.out.println("No grade");
+        }
     }
 }
