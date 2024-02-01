d49a47a15e1b146df3d930c61bdd2ddab35f6e93
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-01 12:16:25 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,37 +1,38 @@
 import java.util.*;
-public class Practice{
-    public static int first =-1;
-    public static int last =-1;
-    public static  void Occurences(String str,int i,char ch)
+public class Practice
+{ 
+    
+    public static int first = -1;
+    public static int last = -1;
+    public static void Occurence(String str,int i,char ch)
     {
         if(i == str.length())
         {
-            System.out.println("First Occurence of "+ch+" at : "+first);
-            System.out.println("Last Occurence of "+ch+" at : "+last);
+            System.out.println(first);
+            System.out.println(last);
             return;
         }
-        // char currentChar = str.charAt(i);
         if(str.charAt(i) == ch)
         {
             if(first == -1)
             {
                 first = i;
             }
-            else{
+            else
+            {
                 last = i;
             }
         }
-        Occurences(str, i+1,ch);
+        Occurence(str, i+1, ch);
     }
-    
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String : ");
+        System.out.println("Enter String : ");
         String str = sc.next();
-        System.out.println("Enter element to be searched");
+        System.out.println("Enter element to be searched : ");
         char ch = sc.next().charAt(0);
-        
-        Occurences(str, 0,ch);
+
+        Occurence(str, 0, ch);
     }
 }
