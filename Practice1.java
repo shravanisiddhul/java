b0dd39a841251808936c5e5655f84a2aa1668ecb
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-01 20:29:59 +0530

    Changes to be committed: 	modified:   Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -3,9 +3,13 @@ public class Practice1
 {
     public static void main(String args [])
     {
-       for(int i=10;i>=1;i--)
+       for(int i=2020;i>=2000;i=i-4)
        {
-        System.out.println(i);
+        if(i % 4 == 0)
+        {
+            System.out.println(i);
+
+        }
        }
 
     }    
