46defbef9ff439c45adc99cda1775ea809a3c495
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-01 10:40:35 +0530

    Changes to be committed: 	deleted:    Count.java 	modified:   Practice.class 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,26 +4,19 @@ public class Practice
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Marks :");
-        int marks = sc.nextInt();
+        System.out.println("Enter your Age :");
+        int age = sc.nextInt();
         
 
-        if(marks >= 90 && marks <= 100)
+        if(age < 0)
         {
-            System.out.println("A grade");
-        }else if(marks >= 80 && marks <= 89)
+            System.out.println("Enter Correct Age");
+        }else if (age < 18)
         {
-            System.out.println(" B grade");
-        }else if (marks >=70 && marks <= 79)
-        {
-            System.out.println("C grade");
+            System.out.println(" Not Eligible");
         }
-        else if(marks >= 60 && marks <= 69)
-        {
-            System.out.println("D grade");
-        }else
-        {
-            System.out.println("No grade");
+        else{
+            System.out.println("Eligible");
         }
     }
 }
