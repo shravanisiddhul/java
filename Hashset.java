82b7e2747d5e09b6b7c43854a3e6cd7b2b186d7a
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-03 12:17:09 +0530

    Changes to be committed: 	new file:   Hashset.java

---
+++ Hashset.java
@@ -0,0 +1,14 @@
+
+public class Hashset<T> {
+
+    public void add(String newString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    public boolean contains(String newString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+}
