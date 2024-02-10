8ab3a8b36646dd45bd44616e1a9888dc499fbcbe
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-10 19:40:15 +0530

    Changes to be committed: 	modified:   Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -4,29 +4,34 @@ public class Practice1
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int [4];
+        System.out.println("Enter size : ");
+        int size =sc.nextInt();
+        int arr[] = new int [size];
         System.out.println("Enter elements in array 1 :");
-        for(int i=0;i<4;i++)
+        for(int i=0;i<size;i++)
         {
             arr[i] = sc.nextInt();
         }
 
-        int arr2[] = new int [4];
+        int arr2[] = new int [size];
         System.out.println("Enter elements in array 2 :");
-        for(int i=0;i<4;i++)
+        for(int i=0;i<size;i++)
         {
             arr2[i] = sc.nextInt();
         }
-    }
-       
-        int result[] = new int [4];
-        for(int i=0;i<4;i++)
+
+        int[] result = new int[size];
+        // int result = new int [size];
+        for(int i=0;i<size;i++)
         {
-            int[] arr;
-            int[] arr2;
             result[i] = arr[i] + arr2[i];
-            System.out.println(result);
         }
 
-
+        System.out.println("The result is : ");
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(result[i]+" ");
+        }
+        
+    }
 }
