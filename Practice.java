4df8d26c8a754488d93deddc9423dadd73ab5b10
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-10 20:33:28 +0530

    hanges to be committed:     modified:   .vscode/settings.json 	modified:   Practice.java 	modified:   practice2.java
     Changes not staged for commit:
    	modified:   .vscode/settings.json

--- Practice.java
+++ Practice.java
@@ -1,27 +1,117 @@
 import java.util.*;
 public class Practice
 {
+    public static boolean isSafe(char[][] board,int row,int col)
+    {
+        int n = board.length;
+        for(int j=0;j<board.length;j++)
+        {
+            if(board[row][j] == 'Q')
+            {
+                return false;
+            }
+        }
+
+        for(int i=0;i<board.length;i++)
+        {
+            if(board[i][col] == 'Q')
+            {
+                return false;
+            }
+        }
+
+        int i = row;
+        int j = col;
+        while(i>=0 && j>=0)
+        {
+            if(board[i][j] == 'Q')
+            {
+                return false;
+            }
+            i--;
+            j--;  
+        }
+
+        i = row;
+        j = col;
+        while(i<n && j>=0)
+        {
+            if(board[i][j] == 'Q')
+            {
+                return false;
+            }
+            i++;
+            j--; 
+        }
+
+        i = row;
+        j = col;
+        while(i>=0 && j<n)
+        {
+            if(board[i][j] == 'Q')
+            {
+                return false;
+            }
+            i--;
+            j++;  
+        }
+
+        i = row;
+        j = col;
+        while(i<n && j<n)
+        {
+            if(board[i][j] == 'Q')
+            {
+                return false;
+            }
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+    public static void nqueen(char[][] board,int row)
+    {
+        int n = board.length;
+        if(row == n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    System.out.print(board[i][j]);
+                }
+                System.out.println();
+            }   
+            System.out.println();
+            return;
+        }
+
+        for(int j=0;j<n;j++)
+        {
+            if(isSafe(board,row,j))
+            {
+                board[row][j] = 'Q';
+                nqueen(board, row+1);
+                board[row][j] = 'X';
+            }
+        }
+    }
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size : ");
-        int size = sc.nextInt();
+        System.out.println("Enter no of size you want of the board : ");
+        int n = sc.nextInt();
 
-        String arr[] = new String[size];
-
-        System.out.println("Enter elements in an array : ");
-        for(int i=0 ;i<size;i++)
+        char [][] board = new char [n][n];
+        for(int i=0;i<n;i++)
         {
-            arr[i] = sc.next();
-        }
-
-        for(int i =0;i<size; i++)
-        {
-            char firstchar = Character.toLowerCase(arr[i].charAt(0));
-            if(firstchar == 'a' || firstchar == 'e' || firstchar == 'i' || firstchar == 'o' || firstchar == 'u')
+            for(int j=0;j<n;j++)
             {
-                System.out.println(arr[i]);
+                board[i][j] = 'X';
             }
         }
+        nqueen(board, 0);
+
     }
 }
--- practice2.java
+++ practice2.java
@@ -4,30 +4,50 @@ public class practice2
     public static void main(String args [])
     {
         Scanner sc = new Scanner(System.in);
-        int arr[][] = new int [2][3];
+        int arr[][][] = new int [1][2][3];
         System.out.println("Enter elements in an array  : ");
-        for(int i=0;i<2;i++)
+        for(int i=0;i<=2;i++)
         {
-            for(int j=0;j<3;j++)
+            for(int j=0;j<2;j++)
             {
-                arr[i][j] = sc.nextInt();
+                for(int k=0;k<=3;k++)
+                {
+                    arr[i][j][k] = sc.nextInt();
+                }
             }
         }
 
-        int total = 0;
-        int count = 0;
-        
+        for(int i=0;i<=2;i++)
+        {
+            for(int j=0;j<=2;j++)
+            {
+                for(int k=0;k<=3;k++)
+                {
+                    System.out.println(arr[i]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        // int total = 0;
+        // int count = 0;
+        int min = 0;
         for(int i=0;i<arr.length;i++)
         {
-            for(int j=0;j<arr[i].length;j++)
+            for(int j=0;j<arr.length;j++)
             {
-                total += arr[i][j]; 
-                count++;
+                for(int k=0;k<arr.length;i++)
+                {
+                    min = Integer.MAX_VALUE;
+                    System.out.println(min);
+                }
             }
+            System.out.println(min);
         }
 
-        double average = total /count;
-        System.out.println("The Average : "+average);
+        // double average = total /count;
+        // System.out.println("The Average : "+average);
         
     }    
 }
