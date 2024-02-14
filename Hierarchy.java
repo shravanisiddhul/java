6b9056d87ee50c4bbcc13636366986a12e6a682f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-14 20:55:06 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Hierarchy.java

---
+++ Hierarchy.java
@@ -0,0 +1,37 @@
+
+//  Hierarchy Inheritance
+class A
+{
+	public void display()
+	{
+		System.out.println("class A");
+	}
+	
+}
+ 
+
+class B extends A
+{
+	public void show()
+	{
+		System.out.println("class B");
+	}
+}
+
+class C extends A
+{
+	public void set()
+	{
+		System.out.println("class C");
+	}
+}
+ 
+public class Hierarchy {
+    public static void main(String args[]){
+        B obj = new B();
+        obj.display();
+        obj.show();
+        C obj2 = new C();
+        obj2.set();
+    }
+}
