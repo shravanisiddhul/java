5fc8dfcf5057fd6231fb15401febb0dfa37808b7
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-19 20:05:48 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -4,68 +4,57 @@ public class Practice
     public static boolean isSafe(char[][] board,int row,int col)
     {
         int n = board.length;
+        // 2 Up 1 left
+        int i= row-2;
+        int j=col-1;
+        if(i>=0 && j>=0 && board[i][j] == 'K')
+            return false;
         
-        // horizontal
-        for(int j=0;j<n;j++)
-        {
-            if(board[row][j] == 'Q')
-            {
-                return false;
-            }
-        }
+        // 2 Up 1 right
+        i= row-2;
+        j=col+1;
+        if(i>=0 && j<n && board[i][j] == 'K')
+        return false;
 
-        // vertically
-        for(int i=0;i<n;i++)
-        {
-            if(board[i][col] == 'Q')
-            {
-                return false;
-            }
-        }
+        // 2 Down 1 left
+        i= row+2;
+        j=col-1;
+        if(i<n && j>=0 && board[i][j] == 'K')
+        return false;
 
-        int i=row;
-        int j=col;
-        while(i<n && j>=0)
-        {
-            if(board[i][j] == 'Q')
-                return false;
-                i++;
-                j--;
-        }
+        // 2 Down 1 right
+        i= row+2;
+        j=col+1;
+        if(i<n && j<n && board[i][j] == 'K')
+        return false;
 
-        i= row;
-        j= col;
-        while(i<n && j<n)
-        {
-            if(board[i][j] == 'Q')
-                return false;
-                i++;
-                j++;
-        }
+        // 2 left 1 up
+        i= row-1;
+        j=col-2;
+        if(i>=0 && j>=0&& board[i][j] == 'K')
+        return false;
 
-        i= row;
-        j= col;
-        while(i>=0 && j<n)
-        {
-            if(board[i][j] == 'Q')
-                return false;
-                i--;
-                j++;
-            
-        }
+        // 2 left 1 down
+        i= row+1;
+        j=col-2;
+        if(i<n && j>=0 && board[i][j] == 'K')
+        return false;
 
-        i= row;
-        j= col;
-        while(i>=0 && j>=0)
-        {
-            if(board[i][j] == 'Q')
-                return false;
-                i--;
-                j--;
-        }
+        // 2 right 1 up
+        i= row-1;
+        j=col+2;
+        if(i>=0 && j<n && board[i][j] == 'K')
+        return false;
+
+        // 2 right 1 down
+        i= row+1;
+        j=col+2;
+        if(i<n && j<n && board[i][j] == 'K')
+        return false;
+
         return true;
     }
-    public static void nqueen(char[][] board,int row)
+    public static void knights(char[][] board,int row,int col,int num)
     {
         int n = board.length;
         if(row == n)
@@ -79,21 +68,33 @@ public class Practice
                 System.out.println();
             }
             System.out.println();
-            return ;
+            return;
         }
-        for(int j=0;j<n;j++)
+        else if(isSafe(board,row,col))
         {
-            if(isSafe(board,row,j))
+            board[row][col] = 'K';
+            if(col != n-1)
             {
-                board[row][j] = 'Q';
-                nqueen(board, row+1);
-                board[row][j] = 'X';
+                knights(board, row, col+1, num+1);
             }
+            else
+            {
+                knights(board, row+1,0, num+1);
+            }
+            board[row][col] = 'X';
         }
+        if(col != n-1)
+        {
+            knights(board, row, col+1,num);
+        }
+        else
+        {
+            knights(board, row+1,0, num);
+        } 
     }
-    public static void main(String args[])
+    public static void main(String args [])
     {
-        int n = 4;
+        int n = 3;
         char[][] board = new char[n][n];
         for(int i=0;i<n;i++)
         {
@@ -102,7 +103,6 @@ public class Practice
                 board[i][j] = 'X';
             }
         }
-        nqueen(board,0);
+        knights(board, 0, 0, 0);
     }
-
 }
