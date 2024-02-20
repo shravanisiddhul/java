f22eb05c40f9173ab2de3eae945130e661ea10c2
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-20 13:26:49 +0530

    Changes to be committed:

--- Practice.java
+++ Practice.java
@@ -1,108 +1,58 @@
 import java.util.*;
 public class Practice
 {
-    public static boolean isSafe(char[][] board,int row,int col)
+    public static int partition(int arr[],int low,int high)
     {
-        int n = board.length;
-        // 2 Up 1 left
-        int i= row-2;
-        int j=col-1;
-        if(i>=0 && j>=0 && board[i][j] == 'K')
-            return false;
+        int pivot = arr[high];
+        int i = low - 1;
         
-        // 2 Up 1 right
-        i= row-2;
-        j=col+1;
-        if(i>=0 && j<n && board[i][j] == 'K')
-        return false;
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
 
-        // 2 Down 1 left
-        i= row+2;
-        j=col-1;
-        if(i<n && j>=0 && board[i][j] == 'K')
-        return false;
-
-        // 2 Down 1 right
-        i= row+2;
-        j=col+1;
-        if(i<n && j<n && board[i][j] == 'K')
-        return false;
-
-        // 2 left 1 up
-        i= row-1;
-        j=col-2;
-        if(i>=0 && j>=0&& board[i][j] == 'K')
-        return false;
-
-        // 2 left 1 down
-        i= row+1;
-        j=col-2;
-        if(i<n && j>=0 && board[i][j] == 'K')
-        return false;
-
-        // 2 right 1 up
-        i= row-1;
-        j=col+2;
-        if(i>=0 && j<n && board[i][j] == 'K')
-        return false;
-
-        // 2 right 1 down
-        i= row+1;
-        j=col+2;
-        if(i<n && j<n && board[i][j] == 'K')
-        return false;
-
-        return true;
+        return i;
     }
-    public static void knights(char[][] board,int row,int col,int num)
+    public static void quickSort(int arr[],int low,int high)
     {
-        int n = board.length;
-        if(row == n)
+        if(low < high)
         {
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    System.out.print(board[i][j]);
-                }
-                System.out.println();
-            }
-            System.out.println();
-            return;
+            int pidx = partition(arr,low,high);
+            quickSort(arr, low, pidx -1);
+            quickSort(arr, pidx+1, high);
+            
         }
-        else if(isSafe(board,row,col))
-        {
-            board[row][col] = 'K';
-            if(col != n-1)
-            {
-                knights(board, row, col+1, num+1);
-            }
-            else
-            {
-                knights(board, row+1,0, num+1);
-            }
-            board[row][col] = 'X';
-        }
-        if(col != n-1)
-        {
-            knights(board, row, col+1,num);
-        }
-        else
-        {
-            knights(board, row+1,0, num);
-        } 
     }
-    public static void main(String args [])
+    public static void main(String args[])
     {
-        int n = 3;
-        char[][] board = new char[n][n];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size : ");
+        int size = sc.nextInt();
+        int arr[] = new int [size];
+        System.out.println("Enter an Array : ");
+        for(int i=0;i< arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int n = arr.length;
+        quickSort(arr, 0, n-1);
+
+        System.out.println();
+        System.out.println("Sorted Array : ");
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                board[i][j] = 'X';
-            }
+            System.out.print(arr[i] + " ");
         }
-        knights(board, 0, 0, 0);
+        System.out.println();
     }
 }
