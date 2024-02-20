0a0dcef0d56e6070f0a7a606ca5f9425a34e8bb8
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-20 14:07:31 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,57 +1,69 @@
 import java.util.*;
 public class Practice
 {
-    public static int partition(int arr[],int low,int high)
+    public static void conquer(int arr[],int si,int mid,int ei)
     {
-        int pivot = arr[high];
-        int i = low - 1;
-        
-        for(int j=low;j<high;j++)
+        int merged[] = new int [ei-si+1];
+
+        int idx1 = si;
+        int idx2 = mid+1;
+        int x = 0;
+
+        while(idx1 <= mid && idx2 <=ei)
         {
-            if(arr[j] < pivot)
+            if(arr[idx1] <= arr[idx2])
             {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                merged[x++] = arr[idx1++];
+            }else{
+                merged[x++] = arr[idx2++];
             }
         }
-        i++;
-        int temp = arr[i];
-        arr[i] = pivot;
-        arr[high] = temp;
 
-        return i;
-    }
-    public static void quickSort(int arr[],int low,int high)
-    {
-        if(low < high)
+        while(idx1 <= mid)
         {
-            int pidx = partition(arr,low,high);
-            quickSort(arr, low, pidx -1);
-            quickSort(arr, pidx+1, high);
-            
+            merged[x++] = arr[idx1++];
+        }
+        while(idx2 <= ei)
+        {
+            merged[x++]= arr[idx2++];
+        }
+
+        for(int i=0,j=si;i<merged.length;i++,j++)
+        {
+            arr[j] = merged[i];
         }
     }
-    public static void main(String args[])
+    public static void divide(int arr[],int si,int ei)
+    {
+        if(si >= ei)
+        {
+            return;
+        }
+        int mid = si+(ei-si)/2;
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
+        conquer(arr, si, mid, ei);
+    }
+    public static void main(String args []) 
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size : ");
         int size = sc.nextInt();
         int arr[] = new int [size];
-        System.out.println("Enter an Array : ");
-        for(int i=0;i< arr.length;i++)
+
+        System.out.println("Enter Array ");
+        for(int i=0;i<size;i++)
         {
             arr[i] = sc.nextInt();
         }
         int n = arr.length;
-        quickSort(arr, 0, n-1);
-
         System.out.println();
-        System.out.println("Sorted Array : ");
+        divide(arr, 0, n-1);
+
+        System.out.println("Sorted Array is : ");
         for(int i=0;i<n;i++)
         {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
