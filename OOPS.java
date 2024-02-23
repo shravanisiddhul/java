7547ccf57c2abd6297fb2586b5966a74062a7eeb
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-02-23 09:53:19 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Constructor.class 	modified:   OOPS.java 	new file:   OOPS1.class 	new file:   OOPS1.java 	modified:   Practice.java 	new file:   Student.class

--- OOPS.java
+++ OOPS.java
@@ -16,6 +16,7 @@ class Pen
     }
 
 }
+
 public class OOPS {
     public static void main(String args [])
     {
--- Practice.java
+++ Practice.java
@@ -1,70 +1,27 @@
-import java.util.*;
-public class Practice
-{
-    public static void conquer(int arr[],int si,int mid,int ei)
-    {
-        int merged[] = new int [ei-si+1];
-
-        int idx1 = si;
-        int idx2 = mid+1;
-        int x = 0;
-
-        while(idx1 <= mid && idx2 <=ei)
-        {
-            if(arr[idx1] <= arr[idx2])
-            {
-                merged[x++] = arr[idx1++];
-            }else{
-                merged[x++] = arr[idx2++];
-            }
-        }
-
-        while(idx1 <= mid)
-        {
-            merged[x++] = arr[idx1++];
-        }
-        while(idx2 <= ei)
-        {
-            merged[x++]= arr[idx2++];
-        }
-
-        for(int i=0,j=si;i<merged.length;i++,j++)
-        {
-            arr[j] = merged[i];
-        }
+class Student {
+    int id;
+    String name;
+  
+    // Constructor
+    Student(int i, String n) {
+      id = i;
+      name = n;
     }
-    public static void divide(int arr[],int si,int ei)
-    {
-        if(si >= ei)
-        {
-            return;
-        }
-        int mid = si+(ei-si)/2;
-        divide(arr, si, mid);
-        divide(arr, mid+1, ei);
-        conquer(arr, si, mid, ei);
+  
+    // Method to display the values
+    void display() {
+      System.out.println(id + " " + name);
     }
-    public static void main(String args []) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size : ");
-        int size = sc.nextInt();
-        int arr[] = new int [size];
-
-        System.out.println("Enter Array ");
-        for(int i=0;i<size;i++)
-        {
-            arr[i] = sc.nextInt();
-        }
-        int n = arr.length;
-        System.out.println();
-        divide(arr, 0, n-1);
-
-        System.out.println("Sorted Array is : ");
-        for(int i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+  }
+  
+  public class Practice {
+    public static void main(String[] args) {
+      // Creating objects and passing values
+      Student s1 = new Student(111, "Karan");
+      Student s2 = new Student(222, "Aryan");
+  
+      // Calling method to display the values of object
+      s1.display();
+      s2.display();
     }
-}
+  }
