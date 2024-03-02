2fa4598fabd99423b905e7b1765366ab7a370c52
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-03-02 09:08:51 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,13 +1,21 @@
 import java.util.*;
+
+// Parameterized Constructor
 class Car
 {
   String color;
   float speed;
 
+  Car(String color, float speed) {
+    
+    this.color = color;
+    this.speed = speed;
+  }
+
   public void carInfo()
   {
-      System.out.println("Color of Car is :"+this.color);
-      System.out.println("Speed of car is :"+this.speed);
+    System.out.println("Color of car is : "+this.color);
+    System.out.println("Speed of color is :"+this.speed);
   }
 }
 public class Practice
@@ -15,16 +23,12 @@ public class Practice
     public static void main(String args [])
     {
         System.out.println("Car 1 Details are :"); 
-        Car C1 = new Car();
-        C1.color = "Purple";
-        C1.speed = 120 ;
+        Car C1 = new Car("Purple",120);
         C1.carInfo();
 
         System.out.println();
         System.out.println("Car 2 Details are :");
-        Car C2 = new Car();
-        C2.color = "Neavy Blue";
-        C2.speed = 125;
+        Car C2 = new Car("Neavy Blue",125);
         C2.carInfo();
         System.out.println();
     }
