599843ea2f3c752932717e358448a4e62768abbf
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-03-10 16:36:00 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	new file:   LL1.java

---
+++ LL1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class LL1{
+
+    public static void main(String args [])
+    {
+       LinkedList<String> list = new LinkedList<>();
+       list.addFirst("a");
+       list.addFirst("is");
+       list.addLast("List");
+       list.addFirst("These");
+       System.out.println(list);
+
+       System.out.println(list.size());
+
+       for(int i=0;i<list.size();i++)
+       {
+            System.out.print(list.get(i)+" -> ");
+       }
+       System.out.println("Null");
+    }
+}
