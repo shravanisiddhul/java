5d4fbcfa02feac236aad4aa29354cee66f2a66c8
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-03-18 14:21:39 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,51 +1,36 @@
-public class Practice {
-    static class Node{
-        int data; 
-        Node next;
+import java.util.ArrayList;
 
-        public Node(int data)
-        {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    static class stack{
-        public static Node head;
-        public boolean isEmpty()
-        {
-            return head == null;
-        }
-        public void push(int data)
-        {
-            Node newNode = new Node(data);
-            if(isEmpty())
-            {
-                head = newNode;
-                return ;
-            }
-            newNode.next = head;
-            head = newNode;
-        }
-        public int pop()
-        {
-            if(isEmpty())
-            {
-                return -1;
-            }
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
-        public int peek()
-        {
-            if(isEmpty())
-            {
-                return -1;
-            }
-            return head.data;
-            
-        }
-    }
+public class Practice {
+	static class stack
+	{
+    final ArrayList<Integer> list = new ArrayList<>();
+	public boolean isEmpty()
+	{
+		return list.size() == 0;
+	}
+	public void push(int data)
+	{
+		list.add(data);
+	}
+	public int pop()
+	{
+		if(isEmpty())
+		{
+			return -1;
+		}
+		int top = list.get(list.size()-1);
+		list.remove(list.size()-1);
+		return top;
+	}
+	public int peek()
+	{
+		if(isEmpty())
+		{
+			return -1;
+		}
+		return list.get(list.size()-1);
+	}
+}
     public static void main(String args [])
     {
         stack s = new stack();
