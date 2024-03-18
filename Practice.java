3c68e4070dde0497677f4bac861cb610382f5b09
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-03-18 13:53:23 +0530

    modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,60 +1,64 @@
-//stack using Linked List
 public class Practice {
-    private static class Node {
+    static class Node{
         int data;
         Node next;
 
-        Node(int data) {
+        public Node(int data)
+        {
             this.data = data;
-            next = null;
+            this.next = null;
         }
     }
-
-    static class Stack {
-        public static Node head = null;
-        public static void push(int data) {
+    static class stack{
+        public static Node head;
+        public boolean isEmpty()
+        {
+            return head == null;
+        }
+        public void push(int data)
+        {
             Node newNode = new Node(data);
-
-            if(head == null) {
+            if(isEmpty())
+            {
                 head = newNode;
-                return;
+                return ;
             }
             newNode.next = head;
             head = newNode;
         }
-
-        public static boolean isEmpty() {
-            return head == null;
-        }
-
-        public static int pop() {
-            if(isEmpty()) {
+        public int pop()
+        {
+            if(isEmpty())
+            {
                 return -1;
             }
-            Node top = head;
+            int top = head.data;
             head = head.next;
-            return top.data;
+            return top;
         }
-
-        public static int peek() {
-            if(isEmpty()) {
+        public int peek()
+        {
+            if(isEmpty())
+            {
                 return -1;
             }
-            Node top = head;
-            return top.data;
+            return head.data;
+            
         }
     }
-
-    public static void main(String args[]) {
-        Stack stack = new Stack();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-
-        while(!stack.isEmpty()) {
-            System.out.println(stack.peek());
-            stack.pop();
+    public static void main(String args [])
+    {
+        stack s = new stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty())
+        {
+            System.out.println(s.peek());
+            s.pop();
         }
+
     }
+
 }
