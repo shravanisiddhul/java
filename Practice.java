ffd94b9ec343800cbc1e737c24a3b54028993d3f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-03-27 11:21:21 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,6 +1,7 @@
 public class Practice
 {
-    static class Queue{
+    static class Queue
+    {
         static int arr[];
         static int size;
         static int rear = -1;
@@ -17,20 +18,20 @@ public class Practice
         }
         public boolean isFull()
         {
-            return (rear + 1)% size == front;
+            return (rear+1)%size == front;
         }
         public void add(int data)
         {
             if(isFull())
             {
                 System.out.println("Queue is Full");
-                return;
+                return ;
             }
             if(front == -1)
             {
                 front = 0;
             }
-            rear = (rear + 1 )%size;
+            rear = (rear+1)%size;
             arr[rear] = data;
 
         }
@@ -42,12 +43,11 @@ public class Practice
                 return -1;
             }
             int result = arr[front];
-            if(rear == front )
+            if(rear == front)
             {
                 rear = front = -1;
-            }
-            else{
-                front = (front + 1) % size;
+            }else{
+                front = (front +1)%size;
             }
             return result;
         }
@@ -63,22 +63,21 @@ public class Practice
     }
     public static void main(String args [])
     {
-        Queue q = new Queue(5);
+        Queue q = new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
         q.add(4);
+        System.out.println(q.remove());
         q.add(5);
-        System.out.println(q.remove());
-        q.add(6);
-        System.out.println(q.remove());
-        q.add(7);
 
         while(!q.isEmpty())
         {
             System.out.println(q.peek());
             q.remove();
-            
+
         }
     }
+
 }
