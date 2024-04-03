ee2db25dd1dd9d55d91b3152a9ef759033120db3
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-03 11:41:02 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,22 +1,20 @@
-import java.math.MathContext;
 import java.util.LinkedList;
 import java.util.Queue;
-public class Practice
+public  class Practice
 {
-    static class Node 
-    {
-        int data;
-        Node left;
-        Node right;
+    static class Node{
+    int data;
+    Node left;
+    Node right;
 
-        Node(int data)
-        {
-            this.data = data;
-            this.left = null;
-            this.right = null; 
-        }
-    } 
-    public static class BinaryTree
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    }
+    static class BinaryTree
     {
         static int idx = -1;
         public static Node builtTree(int nodes[])
@@ -29,7 +27,6 @@ public class Practice
             Node newNode = new Node(nodes[idx]);
             newNode.left = builtTree(nodes);
             newNode.right = builtTree(nodes);
-
             return newNode;
         }
     }
@@ -100,7 +97,8 @@ public class Practice
                     q.add(currNode.right);
                 }
             }
-        } 
+        }
+
     }
     public static int countNodes(Node root)
     {
@@ -117,37 +115,34 @@ public class Practice
     {
         if(root == null)
         {
-            return 0 ;
+            return 0;
         }
         int leftSum = SumOfNodes(root.left);
         int rightSum = SumOfNodes(root.right);
 
-        return leftSum+rightSum+root.data;
+        return leftSum+rightSum+ root.data;
     }
     public static int height(Node root)
     {
         if(root == null)
         {
-            // System.out.print("-1"+" ");
             return 0;
         }
         int leftHeight = height(root.left);
         int rightHeight = height(root.right);
 
-        int  myHeight = Math.max(leftHeight, rightHeight) + 1;
-
-        return myHeight;
+        int maxHeight = Math.max(leftHeight, rightHeight)+1;
+        return maxHeight;
     }
     public static int diameter(Node root)
     {
         if(root == null)
         {
-            // System.out.print("-1"+" ");
             return 0;
         }
-        int diam1 = diameter(root.left);
+        int diam1= diameter(root.left);
         int diam2 = diameter(root.right);
-        int diam3 = height(root.left)+height(root.right) + 1;
+        int diam3 = height(root.left)+height(root.right)+1;
 
         return Math.max(Math.max(diam1, diam2), diam3);
     }
@@ -158,46 +153,49 @@ public class Practice
 
         TreeInfo(int height,int diam)
         {
+            this.height = height;
             this.diam = diam;
-            this.height =  height;
         }
     }
     public static TreeInfo diameter2(Node root)
     {
         if(root == null)
         {
-            return new TreeInfo(0, 0);
-        }
+            return new TreeInfo(0,0);
+        } 
         TreeInfo left = diameter2(root.left);
         TreeInfo right = diameter2(root.right);
-
-        int myHeight = Math.max(left.height, right.height)+1;
+        
+        int maxHeight = Math.max(left.height, right.height) + 1;
 
         int diam1 = left.diam;
         int diam2 = right.diam;
-        int diam3 = left.height+right.height + 1;
+        int diam3 = left.height + right.height + 1;
 
-        int myDiam = Math.max(Math.max(diam2, diam3), diam1);
-        TreeInfo myInfo = new TreeInfo(myHeight, myDiam);
-        return myInfo;
+        int maxDiam = Math.max(Math.max(diam1, diam2), diam3);
+        return new TreeInfo(maxHeight, maxDiam);
     }
-    public static void main(String args [])
+
+    public static void main(String args[])
     {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = BinaryTree.builtTree(nodes);
+        System.out.println("Root Node of a Tree is : ");
         System.out.println(root.data);
-        System.out.println("Pre-order traversal is : ");
+        System.out.println("\npre-order traversal is :");
         preorder(root);
-        System.out.println("\nIn-Order traversal is : ");
+        System.out.println("\nIn-order traversal is :");
         inorder(root);
-        System.out.println("\nPost-Order traversal is : ");
+        System.out.println("\nPost-order traversal is : ");
         postorder(root);
-        System.out.println("\nLevel traversal is : ");
+        System.out.println("\nLevel order traversal is : ");
         levelOrder(root);
-        System.out.println("Count of the Node are : "+countNodes(root));
+        System.out.println("Count of Nodes in a Tree are : "+countNodes(root));
         System.out.println("Sum of Nodes are : "+SumOfNodes(root));
-        System.out.println("Height of tree is : "+height(root));
-        System.out.println("Diameter of tree is : "+diameter(root));
-        System.out.println("Diameter2 of tree is : "+diameter2(root).diam);
+        System.out.println("Height of Tree is : "+height(root));
+        System.out.println("Diameter of Tree : "+diameter(root));
+
+        System.out.println("Diameter 2 of tree is : "+diameter2(root).diam);
     }
+
 }
