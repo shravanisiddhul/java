bd50d741797476796f338be6194867b916a6e823
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-06 09:57:41 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,201 +1,101 @@
-import java.util.LinkedList;
-import java.util.Queue;
-public  class Practice
+public class Practice
 {
-    static class Node{
-    int data;
-    Node left;
-    Node right;
+    Node head;
 
-    Node(int data)
+    class Node
     {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-    }
-    static class BinaryTree
-    {
-        static int idx = -1;
-        public static Node builtTree(int nodes[])
+        int data ;
+        Node next;
+
+        Node(int data)
         {
-            idx++;
-            if(nodes[idx] == -1)
-            {
-                return null;
-            }
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = builtTree(nodes);
-            newNode.right = builtTree(nodes);
-            return newNode;
+            this.data = data;
+            this.next = null; 
         }
     }
-    public static void preorder(Node root)
+    public void addFirst(int data)
     {
-        if(root == null)
+        Node newNode = new Node(data);
+        if(head == null)
         {
-            System.out.print("-1"+" ");
+            head = newNode;
             return ;
         }
-        System.out.print(root.data+" ");
-        preorder(root.left);
-        preorder(root.right);
+        newNode.next = head ;
+        head = newNode;
     }
-    public static void inorder(Node root)
+    public void addLast(int data)
     {
-        if(root == null)
+        Node newNode = new Node(data);
+        if(head == null)
         {
-            System.out.print("-1"+" ");
+            head = newNode;
             return ;
-        }
-        inorder(root.left);
-        System.out.print(root.data+" ");
-        inorder(root.right);
-    }
-    public static void postorder(Node root)
-    {
-        if(root == null)
+        }  
+        Node currNode = head;
+        while(currNode.next != null)
         {
-            System.out.print("-1"+" ");
-            return ;
-        }
-        postorder(root.left);
-        postorder(root.right);
-        System.out.print(root.data+" ");
-    }
-    public static void levelOrder(Node root)
-    {
-        if(root == null)
-        {
-            System.out.print("-1"+" ");
-            return ;
-        }
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
-
-        while(!q.isEmpty())
-        {
-            Node currNode = q.remove();
-            if(currNode == null)
-            {
-                System.out.println();
-                if(q.isEmpty())
-                {
-                    break;
-                }else{
-                    q.add(null);
-                }
-            }else{
-                System.out.print(currNode.data+" ");
-                if(currNode.left != null)
-                {
-                    q.add(currNode.left);
-                }
-                if(currNode.right != null)
-                {
-                    q.add(currNode.right);
-                }
-            }
-        }
-
-    }
-    public static int countNodes(Node root)
-    {
-        if(root == null)
-        {
-            return 0;
-        }
-        int leftNodes = countNodes(root.left);
-        int rightNodes = countNodes(root.right);
-
-        return leftNodes+rightNodes+1;
-    }
-    public static int SumOfNodes(Node root)
-    {
-        if(root == null)
-        {
-            return 0;
-        }
-        int leftSum = SumOfNodes(root.left);
-        int rightSum = SumOfNodes(root.right);
-
-        return leftSum+rightSum+ root.data;
-    }
-    public static int height(Node root)
-    {
-        if(root == null)
-        {
-            return 0;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-
-        int maxHeight = Math.max(leftHeight, rightHeight)+1;
-        return maxHeight;
-    }
-    public static int diameter(Node root)
-    {
-        if(root == null)
-        {
-            return 0;
-        }
-        int diam1= diameter(root.left);
-        int diam2 = diameter(root.right);
-        int diam3 = height(root.left)+height(root.right)+1;
-
-        return Math.max(Math.max(diam1, diam2), diam3);
-    }
-    static class TreeInfo
-    {
-        int height;
-        int diam;
-
-        TreeInfo(int height,int diam)
-        {
-            this.height = height;
-            this.diam = diam;
-        }
-    }
-    public static TreeInfo diameter2(Node root)
-    {
-        if(root == null)
-        {
-            return new TreeInfo(0,0);
+            currNode = currNode.next;
         } 
-        TreeInfo left = diameter2(root.left);
-        TreeInfo right = diameter2(root.right);
-        
-        int maxHeight = Math.max(left.height, right.height) + 1;
-
-        int diam1 = left.diam;
-        int diam2 = right.diam;
-        int diam3 = left.height + right.height + 1;
-
-        int maxDiam = Math.max(Math.max(diam1, diam2), diam3);
-        return new TreeInfo(maxHeight, maxDiam);
+        currNode.next = newNode;
     }
-
+    public void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+            return ;
+        }
+        Node currNode = head;
+        while(currNode!= null)
+        {
+            System.out.print(currNode.data+" -> ");
+            currNode = currNode.next;
+        }  
+        System.out.println("Null");
+    }
+    public void deleteFirst()
+    {
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+            return ;
+        }
+        head = head.next;
+    }
+    public void deletelast()
+    {
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+            return ;
+        }
+        if(head.next == null)
+        {
+            head = null;
+            return ;
+        }
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null)
+        {
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
     public static void main(String args[])
     {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        Node root = BinaryTree.builtTree(nodes);
-        System.out.println("Root Node of a Tree is : ");
-        System.out.println(root.data);
-        System.out.println("\npre-order traversal is :");
-        preorder(root);
-        System.out.println("\nIn-order traversal is :");
-        inorder(root);
-        System.out.println("\nPost-order traversal is : ");
-        postorder(root);
-        System.out.println("\nLevel order traversal is : ");
-        levelOrder(root);
-        System.out.println("Count of Nodes in a Tree are : "+countNodes(root));
-        System.out.println("Sum of Nodes are : "+SumOfNodes(root));
-        System.out.println("Height of Tree is : "+height(root));
-        System.out.println("Diameter of Tree : "+diameter(root));
-
-        System.out.println("Diameter 2 of tree is : "+diameter2(root).diam);
+        Practice list = new Practice();
+        list.addFirst(2);
+        list.addLast(3);
+        list.addFirst(1);
+        list.addLast(4);
+        list.addFirst(0);
+        list.addLast(5);
+        list.printList();
+        list.deleteFirst();
+        list.deletelast();
+        list.printList();
     }
-
 }
