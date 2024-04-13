b7a93f9f70d63c12e9b05ebddde0325e9fa51226
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-13 10:47:28 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   Hashing.java

---
+++ Hashing.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+public class Hashing {
+    public static void main(String args [])
+    {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1); //it does not allow duplicate values
+        set.add(4);
+        set.add(2);
+
+        // print set
+        System.out.println(set);
+
+        // size
+        System.out.println("Size of set is : "+set.size());
+
+        // contain 
+        if(set.contains(1))
+        {
+            System.out.println("Set contains 1");
+        }else if(!set.contains(6))
+        {
+            System.out.println("Does not Contain");
+        }
+
+        // delete
+        set.remove(4);
+        System.out.println(set);
+        System.out.println("Size of set is : "+set.size());
+    }
+}
