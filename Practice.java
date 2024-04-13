efa7f70c0b5b4754f2597542d4297c001d105d6c
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-13 11:03:15 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,168 +1,35 @@
-import java.util.ArrayList;
-
+import java.util.HashSet;
+import java.util.Iterator;
 public class Practice
 {
-    static class Node
+    public static void main(String args [])
     {
-        int data ;
-        Node left;
-        Node right;
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
 
-        Node(int data)
+        System.out.println(set);
+        System.out.println("Size is : "+set.size());
+
+        if(set.contains(3))
         {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
-    public static Node insert(Node root,int val)
-    {
-        if(root == null)
-        {
-            root = new Node(val);
-        }
-        if(root.data > val)
-        {
-            root.left = insert(root.left, val);
-        }else if(root.data < val){
-            root.right = insert(root.right, val);
-        }
-        return root;
-    }
-    public static void inorder(Node root)
-    {
-        if(root == null)
-        {
-            return ;
-        }
-        inorder(root.left);
-        System.out.print(root.data+" ");
-        inorder(root.right);
-    }
-    public static boolean search(Node root,int val)
-    {
-        if(root == null)
-        {
-            return false;
-        }
-        if(root.data > val)
-        {
-            return search(root.left, val);
-        }else if(root.data == val)
-        {
-            return true;
+            System.out.println("Contains 3");
         }else{
-            return search(root.right, val);
+            System.out.println("Not contains");
         }
-    }
-    public static Node delete(Node root,int val)
-    {
-        if(root == null)
+
+        set.remove(4);
+        System.out.println(set);
+        System.out.println("Size is : "+set.size());
+
+        Iterator it = set.iterator();
+        while(it.hasNext())
         {
-            return null;
-        }
-        if(root.data > val)
-        {
-            root.left = delete(root.left, val);
-        }else if (root.data < val)
-        {
-            root.right = delete(root.right, val);
-        }else{
-            // case 1 
-            if(root.left == null && root.right == null)
-            {
-                return null;
-            }
-            // case 2
-            if(root.left == null)
-            {
-                return root.right;
-            }else if(root.right == null)
-            {
-                return root.left;
-            }
-            // case 3
-            Node IS = inorderSuccesor(root.right);
-            root.data = IS.data;
-            root.right = delete(root.right, IS.data);
-        }
-        return root;
-    }
-    public static Node inorderSuccesor(Node root)
-    {
-        while(root.right != null)
-        {
-            return root.right;
-        }
-        return root;
-    }
-    public static void printInRange(Node root,int x,int y)
-    {
-        if(root == null)
-        {
-            return ;
-        }
-        if(root.data >= x && root.data <= y)
-        {
-            printInRange(root.left, x, y);
-            System.out.print(root.data +" ");
-            printInRange(root.right, x, y);
-        } else if (root.data >= y)
-        {
-            printInRange(root.left, x, y);
-        }else{
-            printInRange(root.right, x, y);
-        } 
-    }
-    public static void printRootToLeaf(Node root,ArrayList<Integer> path)
-    {
-        if(root == null)
-        {
-            return ;
-        }
-        path.add(root.data);
-        if(root.left == null && root.right == null)
-        {
-            printPath(path);
-        }else {
-            printRootToLeaf(root.left, path);
-            printRootToLeaf(root.right, path);
-        }
-        path.remove(path.size()-1);
-    }
-    public static void printPath(ArrayList<Integer>path)
-    {
-        for(int i=0;i<path.size();i++)
-        {
-            System.out.print(path.get(i)+" -> ");
+            System.out.println(it.next());
         }
         System.out.println("Null");
     }
-    public static void main(String args [])
-    {
-        int values[] = {8 ,5, 3, 1, 4, 6,10, 11, 14};
-        Node root = null;
-        for(int i=0; i<values.length;i++)
-        {
-            root = insert(root, values[i]);
-        }
 
-        inorder(root);
-        System.out.println();
-
-        if(search(root, 8))
-        {
-            System.out.println("FOund");
-        }else{
-            System.out.println("Not Found");
-        }
-
-        delete(root, 8);
-        inorder(root);
-
-        System.out.println();
-        printInRange(root, 3, 10);
-        System.out.println();
-        printRootToLeaf(root, new ArrayList<>());
-    }
 }
