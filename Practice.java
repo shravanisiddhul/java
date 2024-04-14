615a3bd8d66ca8a86f962e7d9ba2ee9407993f59
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-14 09:21:08 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,35 +1,26 @@
 import java.util.HashSet;
-import java.util.Iterator;
+
 public class Practice
 {
     public static void main(String args [])
     {
         HashSet<Integer> set = new HashSet<>();
+
         set.add(1);
         set.add(2);
         set.add(3);
         set.add(4);
-
         System.out.println(set);
-        System.out.println("Size is : "+set.size());
+        System.out.println("Size of Set is : "+set.size());
 
-        if(set.contains(3))
-        {
-            System.out.println("Contains 3");
+        if (set.contains(3)) {
+            System.out.println("Contain 3");
         }else{
-            System.out.println("Not contains");
+            System.out.println("Does not Contain");
         }
 
         set.remove(4);
         System.out.println(set);
-        System.out.println("Size is : "+set.size());
-
-        Iterator it = set.iterator();
-        while(it.hasNext())
-        {
-            System.out.println(it.next());
-        }
-        System.out.println("Null");
+        System.out.println("Size of Set is : "+set.size());
     }
-
 }
