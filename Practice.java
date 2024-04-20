4d569ff9e600be481d9308944c5ec342c3713497
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-20 21:43:01 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,191 +1,31 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-public class Practice
+import java.util.HashMap;
+public class Practice 
 {
-    static class hashmap<K,V>
+    public static void majority(int nums[])
     {
-        private class Node{
-            K key;
-            V value;
-
-            Node(K key,V value)
-            {
-                this.key = key;
-                this.value = value;
-            }
-        }
-        private int n;
-        private int N;
-        private LinkedList<Node> buckets[];
-
-        @SuppressWarnings("unchecked")
-        hashmap()
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++)
         {
-            this.N = 4;
-            this.buckets = new LinkedList[4];
-
-            for(int i=0;i<4;i++)
+            if(map.containsKey(nums[i]))
             {
-                buckets[i] = new LinkedList<>();
-            }
-        }
-
-        public int hashFunction( K key)
-        {
-            int bi = key.hashCode();
-            return Math.abs(bi) % N;
-        }
-
-        public int searchInLL(K key,int bi)
-        {
-            LinkedList<Node> ll = buckets[bi];
-            for(int i=0;i<ll.size();i++)
-            {
-                if(ll.get(i).key == key)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        @SuppressWarnings("unchecked")
-        public void rehash()
-        {
-            LinkedList<Practice.hashmap<K, V>.Node>[] oldbuckets = buckets;
-            buckets = new LinkedList[N*2];
-
-            for(int i=0;i<N*2;i++)
-            {
-                buckets[i] = new LinkedList<>();
-            }
-
-            for(int i=0;i<oldbuckets.length;i++)
-            {
-                LinkedList<Node> ll = oldbuckets[i];
-                for(int j=0;j<ll.size();j++)
-                {
-                    Node node = ll.get(j);
-                    put(node.key, node.value);
-                }
-            }
-        }
-
-        public void put(K key,V value)
-        {
-            int bi = hashFunction(key);
-            int di = searchInLL(key, bi);
-
-            if(di == -1)
-            {
-                buckets[bi].add(new Node(key, value));
-                n++;
+                map.put(nums[i], map.get(nums[i])+1);
             }else{
-                Node node = buckets[bi].get(di);
-                node.value = value;
-            }
-
-            double lambda = (double) n / N;
-
-            if(lambda > 2.0)
-            {
-                rehash();
+                map.put(nums[i], 1);
             }
         }
 
-        public boolean containsKey(K key)
+        for(int key : map.keySet())
         {
-            int bi = hashFunction(key);
-            int di = searchInLL(key,bi);
-
-            if(di == -1)
+            if(map.get(key) > n/3)
             {
-                return false;
-            }else{
-               return true;
+                System.out.println(key);
             }
-        }
-
-        public V remove(K key)
-        {
-            
-            int bi = hashFunction(key);
-            int di = searchInLL(key,bi);
-
-            if(di == -1)
-            {
-                return null;
-            }else{
-                Node node = buckets[bi].remove(di);
-                n--;
-                return node.value ;
-            }
-        }
-        public V get(K key)
-        {
-            int bi = hashFunction(key);
-            int di = searchInLL(key,bi);
-
-            if(di == -1)
-            {
-                return null;
-            }else{
-                Node node = buckets[bi].get(di);
-
-                return node.value ;
-            }
-        }
-        public boolean isEmpty()
-        {
-            return n == 0;
-        }
-
-        public ArrayList <K> keyset()
-        {
-            ArrayList<K> keys = new ArrayList<>();
-
-            for(int i=0;i<buckets.length;i++)
-            {
-                LinkedList<Node> ll = buckets[i];
-                for(int j=0 ;j<ll.size();j++)
-                {
-                    Node node = ll.get(j);
-                    keys.add(node.key);
-                }
-            }
-            return keys;
-
         } 
-       
     }
-
-    public static void main(String args[])
+    public static void main(String args [])
     {
-        hashmap<Integer,String> map = new hashmap<>();
-        map.put(1,"Shravani");
-        map.put(2, "Mahi");
-        map.put(3, "Vijju");
-        map.put(4, "Nanda");
-        map.put(5, "Shrushti");
-
-        // to print the hashmap
-        ArrayList<Integer> keys = map.keyset();
-        for(int i=0;i<keys.size();i++)
-        {
-            System.out.println(keys.get(i)+" : "+map.get(keys.get(i)));
-        }
-        // delete
-        map.remove(5);
-        System.out.println();
-
-        // to get the key
-        System.out.println(map.get(5));
-
-        // to print hashmap after deletion
-        for(int i=0 ;i<keys.size();i++)
-        {
-            System.out.println(keys.get(i)+" : "+map.get(keys.get(i)));
-        }
+        int nums[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        majority(nums);
     }
 }
