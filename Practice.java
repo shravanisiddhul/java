67d3f0576b521573a45c18c4f3136c1c7c507c94
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-22 19:54:29 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -19,11 +19,34 @@ public class Practice
         System.out.println(set);
         return set.size();
     }
+
+    public static int intersection(int arr1[], int arr2[])
+    {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<arr1.length;i++)
+        {
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+        for(int i=0;i<arr2.length;i++)
+        {
+            if(set.contains(arr2[i]))
+            {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        return count;
+    }
     public static void main(String args[])
     {
         int arr1[] = { 1, 2, 4, 5, 2};
         int arr2[] = { 1, 3, 4, 6, 7};
 
         System.out.println(union(arr1, arr2));
+
+        System.out.println(intersection(arr1, arr2));
     }
 }
