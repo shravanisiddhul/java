57a2c84a888d101100df8db80a75c9fb71e05ec9
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-22 20:03:08 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,52 +1,41 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Practice
 {
-    public static int union(int arr1[], int arr2[])
+    public static String getStart(HashMap<String ,String> map)
     {
-        HashSet<Integer> set = new HashSet<>();
+        HashMap<String ,String > revMap = new HashMap<>();
 
-        for(int i=0;i<arr1.length;i++)
+        for(String key : map.keySet())
         {
-            set.add(arr1[i]);
+            revMap.put(map.get(key), key);
         }
 
-        for(int i=0;i<arr2.length;i++)
+        for(String key : map.keySet())
         {
-            set.add(arr2[i]);
-        }
-
-        System.out.println(set);
-        return set.size();
-    }
-
-    public static int intersection(int arr1[], int arr2[])
-    {
-        HashSet<Integer> set = new HashSet<>();
-
-        for(int i=0;i<arr1.length;i++)
-        {
-            set.add(arr1[i]);
-        }
-
-        int count = 0;
-        for(int i=0;i<arr2.length;i++)
-        {
-            if(set.contains(arr2[i]))
+            if(!revMap.containsKey(key))
             {
-                count++;
-                set.remove(arr2[i]);
+                return key;
             }
         }
-        return count;
+        return null;
     }
+    
     public static void main(String args[])
     {
-        int arr1[] = { 1, 2, 4, 5, 2};
-        int arr2[] = { 1, 3, 4, 6, 7};
+        HashMap<String ,String > map = new HashMap<>();
+        map.put("Chennai", "Bangaluru");
+        map.put("Mumbai", "Delhi");
+        map.put("Goa", "Chennai");
+        map.put("Delhi", "Goa");
 
-        System.out.println(union(arr1, arr2));
+        String start = getStart(map);
 
-        System.out.println(intersection(arr1, arr2));
+        while(map.containsKey(start))
+        {
+            System.out.print(start+" -> ");
+            start = map.get(start);
+        }
+        System.out.println(start);
     }
 }
