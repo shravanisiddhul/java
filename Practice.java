1a5d0d09f6898712523476b38140b07a7f2f5b9f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-22 19:49:18 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,32 +1,29 @@
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Practice
 {
-    public static void majority(int nums[])
+    public static int union(int arr1[], int arr2[])
     {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arr1.length;i++)
         {
-            if(map.containsKey(nums[i]))
-            {
-                map.put(nums[i], map.get(nums[i])+1);
-            }else{
-                map.put(nums[i], 1);
-            }
+            set.add(arr1[i]);
         }
 
-        for(int key : map.keySet())
+        for(int i=0;i<arr2.length;i++)
         {
-            if (map.get(key) > n/3) {
-                System.out.println(key);
-            }
+            set.add(arr2[i]);
         }
+
+        System.out.println(set);
+        return set.size();
     }
-    public static void main(String args [])
+    public static void main(String args[])
     {
-        int nums[] = {1, 2};
-        majority(nums);
+        int arr1[] = { 1, 2, 4, 5, 2};
+        int arr2[] = { 1, 3, 4, 6, 7};
+
+        System.out.println(union(arr1, arr2));
     }
 }
