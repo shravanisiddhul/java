4c05db1afc1cbf95e05ed5fba8adbdd276fdf667
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-23 19:59:42 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -7,22 +7,22 @@ public class Practice
         int arr[] = {10, 2, -2, -20, 10};
         int k = -10;
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-
+        HashMap<Integer, Integer> map = new HashMap<>();
         int ans = 0;
         int sum = 0;
+
         for(int i=0;i<arr.length;i++)
         {
             sum += arr[i];
 
-            if(map.containsKey(sum-k))
+            if(map.containsKey(sum - k))
             {
-                ans += map.get(sum-k);
+                ans += map.get(sum - k);
             }
 
             if(map.containsKey(sum))
             {
-                map.put(sum,  map.get(sum)+1);
+                map.put(sum, map.get(sum)+1);
             }else{
                 map.put(sum, 1);
             }
