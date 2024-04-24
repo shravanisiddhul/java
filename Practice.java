036db29333ab80da182a8cf8dcca2132930cfbfe
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-24 10:53:38 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,54 +1,41 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Practice
 {
-    public static int union(int arr1[],int arr2[])
+    public static String getStart(HashMap<String, String> ticket)
     {
-        HashSet<Integer> set = new HashSet<>();
+        HashMap<String, String> revMap = new HashMap<>();
 
-        for(int i=0;i<arr1.length;i++)
+        for(String key : ticket.keySet())
         {
-            set.add(arr1[i]);
+            revMap.put(ticket.get(key), key);
         }
 
-        for(int i=0;i<arr2.length;i++)
+        for(String key : ticket.keySet())
         {
-            set.add(arr2[i]);
-        }
-
-        System.out.println(set);
-        return set.size();
-    }
-
-    public static int intersection(int arr1[],int arr2[])
-    {
-        HashSet<Integer> set = new HashSet<>();
-
-        for(int i=0;i<arr1.length;i++)
-        {
-            set.add(arr1[i]);
-        }
-
-        int count = 0;
-        for(int i=0;i<arr2.length;i++)
-        {
-            if(set.contains(arr2[i]))
+            if(!revMap.containsKey(key))
             {
-                count++;
-                set.remove(arr2[i]);
+                return key;
             }
-            // System.out.println(set);
         }
-        // System.out.println(set);
-        return count ;
+        return null;
     }
     public static void main(String args [])
     {
-        int arr1[] = {1, 2, 3, 1, 5, 7, 6};
-        int arr2[] = {3, 7, 3, 9, 8, 4};
+        HashMap<String, String> ticket = new HashMap<>();
+        
+        ticket.put("Chennai", "Bangaluru");
+        ticket.put("Mumbai", "Delhi");
+        ticket.put("Goa", "Chennai");
+        ticket.put("Delhi", "Goa");
 
-        System.out.println(union(arr1, arr2));
-
-        System.out.println(intersection(arr1, arr2));
+        String start = getStart(ticket);
+        
+        while(ticket.containsKey(start))
+        {
+            System.out.print(start +" -> ");
+            start = ticket.get(start);
+        }
+        System.out.println(start);
     }
 }
