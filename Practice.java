3741469b46db0e96d78c5b877503f4e9f34bd73f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-24 11:07:10 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -2,40 +2,33 @@ import java.util.HashMap;
 
 public class Practice
 {
-    public static String getStart(HashMap<String, String> ticket)
-    {
-        HashMap<String, String> revMap = new HashMap<>();
-
-        for(String key : ticket.keySet())
-        {
-            revMap.put(ticket.get(key), key);
-        }
-
-        for(String key : ticket.keySet())
-        {
-            if(!revMap.containsKey(key))
-            {
-                return key;
-            }
-        }
-        return null;
-    }
+   
     public static void main(String args [])
     {
-        HashMap<String, String> ticket = new HashMap<>();
-        
-        ticket.put("Chennai", "Bangaluru");
-        ticket.put("Mumbai", "Delhi");
-        ticket.put("Goa", "Chennai");
-        ticket.put("Delhi", "Goa");
+        int arr[] = {10, 2, -2, -20, 10};
+        int k = -10;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        String start = getStart(ticket);
-        
-        while(ticket.containsKey(start))
+        map.put(0, 1);
+        int ans = 0;
+        int sum = 0;
+        for(int i=0;i<arr.length;i++)
         {
-            System.out.print(start +" -> ");
-            start = ticket.get(start);
+            sum += arr[i];
+
+            if(map.containsKey(sum-k))
+            {
+                ans += map.get(sum-k);
+            }
+
+            if(map.containsKey(sum))
+            {
+                map.put(sum, map.get(sum)+1);
+            }else{
+                map.put(sum, 1);
+            }
+
         }
-        System.out.println(start);
+        System.out.println(ans);
     }
 }
