3f93bbcf86c32d7e653e26e3973c363bb5a4531b
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-24 10:27:23 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -2,34 +2,33 @@ import java.util.HashMap;
 
 public class Practice
 {
-    public static void majority(int arr[])
+    public static void majority(int nums[])
     {
         HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
 
-        int n = arr.length;
         for(int i=0;i<n;i++)
         {
-            if(map.containsKey(arr[i]))
+            if(map.containsKey(nums[i]))
             {
-                map.put(arr[i], map.get(arr[i])+1);
+                map.put(nums[i], map.get(nums[i])+1);
             }else{
-                map.put(arr[i], 1);
+                map.put(nums[i], 1);
             }
-            
         }
 
-        for(int  key : map.keySet())
+        for(int key : map.keySet())
         {
-            if(map.get(key) > n/4)
+            if(map.get(key) > n/3)
             {
                 System.out.println(key);
             }
         }
-
+        
     }
     public static void main(String args [])
     {
-        int arr[] = {1, 2, 1, 2, 4, 5, 3, 9, 1, 1, 2, 2, 1};
-        majority(arr);
+        int nums[] = {1, 2};
+        majority(nums);
     }
 }
