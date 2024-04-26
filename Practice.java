11570628a082505dc3722d0e697659b5f9a2d89a
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-04-26 19:38:50 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,6 +1,6 @@
-public class Practice{
-    static class Node
-    {
+public class Practice
+{
+    static class Node{
         Node[] children;
         boolean eow;
 
@@ -13,11 +13,9 @@ public class Practice{
             }
             eow = false;
         }
-
     }
-
     static Node root = new Node();
-    
+
     public static void insert(String word)
     {
         Node curr = root;
@@ -25,8 +23,7 @@ public class Practice{
         {
             int idx = word.charAt(i) - 'a';
 
-            if(curr.children[idx] == null)
-            {
+            if (curr.children[idx] == null) {
                 curr.children[idx] = new Node();
             }
 
@@ -37,6 +34,7 @@ public class Practice{
 
             curr = curr.children[idx];
         }
+        
     }
 
     public static boolean search(String key)
@@ -59,18 +57,15 @@ public class Practice{
         }
         return true;
     }
-
-    public static void main(String args [])
+    public static void main(String args[])
     {
-        String words[] = {"the ","a","there","their","any"};
+        String words[] = {"the","a","there","their","any"};
         for(int i=0;i<words.length;i++)
         {
             insert(words[i]);
         }
-
         System.out.println(search("their"));
-        System.out.println(search("thor"));
         System.out.println(search("an"));
-
+        System.out.println(search("thor"));
     }
 }
