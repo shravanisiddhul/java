fa2217865f0f59ae723af0a0d0ced2412acc1f94
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-02 20:03:14 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -5,8 +5,7 @@ public class Practice
         Node children[];
         boolean eow;
 
-        Node()
-        {
+        Node(){
             children = new Node[26];
             for(int i=0;i<26;i++)
             {
@@ -23,7 +22,7 @@ public class Practice
         Node curr = root;
         for(int i=0;i<word.length();i++)
         {
-            int idx = word.charAt(i) - 'a';
+            int idx = word.charAt(i)-'a';
 
             if(curr.children[idx] == null)
             {
@@ -51,47 +50,10 @@ public class Practice
                 return false;
             }
 
-            if(i == key.length()-1 && curr.children[idx].eow == false) 
+            if(i == key.length() -1 && curr.children[idx].eow == false)
             {
                 return false;
-            }
-
-            curr = curr.children[idx];
-        }
-        return true;
-    }
-
-    public static boolean wordbreak(String key)
-    {
-        if(key.length() == 0)
-        {
-            return true;
-        }
-
-        for(int i=1;i<=key.length();i++)
-        {
-            String firstPart = key.substring(0, i);
-            String secPart = key.substring(i);
-
-            if(search(firstPart) && wordbreak(secPart))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean startwith(String prefix)
-    {
-        Node curr = root;
-        for(int i=0;i<prefix.length();i++)
-        {
-            int idx = prefix.charAt(i) - 'a';
-
-            if(curr.children[idx] == null)
-            {
-                return false;
-            }
+            } 
             curr = curr.children[idx];
         }
         return true;
@@ -99,20 +61,14 @@ public class Practice
 
     public static void main(String args [])
     {
-        String words[] = {"i","like","mobile","sam","samsung"};
-        String key = "ilikesamsung";
-        String prefix = "sam";
-
+        String words[] = {"i","love","bts","like","mobile"};
         for(int i=0;i<words.length;i++)
         {
             insert(words[i]);
         }
 
-        System.out.println(search("mobile"));
-        System.out.println(search("sung"));
-
-        System.out.println(wordbreak(key));
-
-        System.out.println(startwith(prefix));
+        System.out.println(search("love"));
+        System.out.println(search("bts"));
+        System.out.println(search("mobi"));
     }
 }
