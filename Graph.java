602d4eb85c73982a35e58eef01b102923ab2e9aa
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-05 11:55:42 +0530

    Changes to be committed: 	modified:   Graph.java

--- Graph.java
+++ Graph.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Graph 
+public class Graph
 {
     static class Edge
     {
@@ -12,7 +12,7 @@ public class Graph
             this.src = s;
             this.dest = d;
         }
-    } 
+    }
 
     public static void createGraph(ArrayList<Edge> graph[])
     {
@@ -20,28 +20,37 @@ public class Graph
         {
             graph[i] = new ArrayList<Edge>();
         }
+
         graph[0].add(new Edge(0, 2));
+
         graph[1].add(new Edge(1, 2));
         graph[1].add(new Edge(1, 3));
+        
         graph[2].add(new Edge(2, 0));
         graph[2].add(new Edge(2, 1));
         graph[2].add(new Edge(2, 3));
+
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 2));
     }
-    @SuppressWarnings("unchecked")
+
     public static void main(String args [])
     {
         int V = 4;
-        @SuppressWarnings("unused")
+        @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
 
-        // print 2's neighbours
-        for(int i=0; i<graph[2].size(); i++)
+        // print connected vertices of each vertex
+        for(int i=0; i<graph.length; i++)
         {
-            Edge e = graph[2].get(i);
-            System.out.print(e.dest+" ");
-        }        
+            System.out.println("Vertex "+i+" is connected to : ");
+            for(int j=0; j<graph[i].size();j++)
+            {
+                Edge e = graph[i].get(j);
+                System.out.print(e.dest+" ");
+            }
+            System.out.println();
+        }
     }
 }
