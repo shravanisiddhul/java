d1dd521d74e1c9d7fcac027a186a35b3235a375f
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-05 17:47:04 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Practice
 {
@@ -6,13 +8,11 @@ public class Practice
     {
         int src;
         int dest;
-        int weight;
 
-        Edge(int s,int d,int w)
+        Edge(int s,int d)
         {
             this.src = s;
             this.dest = d;
-            this.weight = w;
         }
     }
 
@@ -22,21 +22,57 @@ public class Practice
         {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 2, 2));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 2, 5));
-        graph[1].add(new Edge(1, 3, 0));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2, 1, 5));
-        graph[2].add(new Edge(2, 3, 10));
-        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 0));
 
-        graph[3].add(new Edge(3, 1, 0));
-        graph[3].add(new Edge(3, 2, 10));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+
+    }
+
+    public static void bfs(ArrayList<Edge> graph[],int V)
+    {
+        Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[V];
+
+        q.add(0);
+
+        while(!q.isEmpty())
+        {
+            int curr = q.remove();
+            if(visited[curr] == false)
+            {
+                System.out.print(curr+" ");
+                visited[curr] = true;
+
+                for(int i=0;i<graph[curr].size();i++)
+                {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
     }
     public static void main(String argsb[])
     {
-        int V = 4;
+        int V = 7;
         @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[V];
         creategraph(graph);
@@ -47,10 +83,11 @@ public class Practice
             for(int j=0;j<graph[i].size();j++)
             {
                 Edge e = graph[i].get(j);
-                System.out.print("("+e.src+", "+e.dest+", "+e.weight+")");
+                System.out.print("("+e.src+", "+e.dest+")");
                 System.out.println(" ");
             }
             System.out.println();
         }
+        bfs(graph, V);
     }
 }
