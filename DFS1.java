c7ecd11dd8e7f3e2f675782c3050687042647986
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-06 11:47:43 +0530

    Changes to be committed:     modified:   .vscode/launch.json 	modified:   DFS.java 	new file:   DFS1.java

--- DFS1.java
+++ DFS1.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
-public class DFS {
-    static class Edge{
+public class DFS1 
+{
+    static class Edge
+    {
         int src ;
         int dest;
 
@@ -10,7 +12,8 @@ public class DFS {
             this.src = s;
             this.dest = d;
         }
-    } 
+    }
+
     public static void createGraph(ArrayList<Edge> graph[])
     {
         for(int i=0;i<graph.length;i++)
@@ -19,34 +22,22 @@ public class DFS {
         }
 
         graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-
+        
         graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
-
-        graph[3].add(new Edge(3, 1));
+        graph[2].add(new Edge(2, 1));
+        
         graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
 
-        graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
-
-        graph[6].add(new Edge(6, 5));
     }
 
     public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[])
     {
         System.out.print(curr+" ");
         vis[curr] = true;
-
+        
         for(int i=0;i<graph[curr].size();i++)
         {
             Edge e = graph[curr].get(i);
@@ -58,30 +49,28 @@ public class DFS {
     }
     public static void main(String args [])
     {
-        int V = 7;
+        int V = 5;
     /*     
-          1 --- 3
-        /      | \
-       0       |  5 -- 6
-        \      | /
-         2 ---- 4
+        1 -- 2 -- 3
+
+           4 -- 5
 
     */
         @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[V];
 
         createGraph(graph);
+
         boolean vis[] = new boolean[V];
-
-        // for(int i=0;i<V;i++)
-        // {
-        //     if(vis[i] == false)
-        //     {
-        //         dfs(graph, i, vis);
-        //     }
-        // }
-        dfs(graph, 0, vis);
-        System.out.println();
+        for(int i=0;i<V ; i++)
+        {
+            if(vis[i] == false)
+            {
+                dfs(graph, i, vis);
+            }
+        }
     }
-
 }
+
+
+
