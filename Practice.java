c2fd4cb2703d50e1055df87a5ebd6d6f48ed5b2b
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-08 20:16:23 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class Practice
 {
-    static class Edge
-    {
-        int src;
+    static class Edge{
+        int src ;
         int dest;
 
         Edge(int s,int d)
         {
-            this.src = s ;
-            this.dest = d ;
+            this.src = s;
+            this.dest = d;
+
         }
     }
 
-    public static void create(ArrayList<Edge> graph[])
+    public static void createGraph(ArrayList<Edge> graph[])
     {
         for(int i=0;i<graph.length;i++)
         {
@@ -26,8 +24,8 @@ public class Practice
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 3));
         graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
 
         graph[2].add(new Edge(2, 0));
         graph[2].add(new Edge(2, 4));
@@ -37,8 +35,8 @@ public class Practice
         graph[3].add(new Edge(3, 5));
 
         graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 5));
         graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
 
         graph[5].add(new Edge(5, 3));
         graph[5].add(new Edge(5, 4));
@@ -47,52 +45,15 @@ public class Practice
         graph[6].add(new Edge(6, 5));
     }
 
-    public static void bfs(ArrayList<Edge> graph[], int V,boolean vis[], int start)
-    {
-        Queue<Integer> q = new LinkedList<>();
-        
-
-        q.add(start);
-
-        while(!q.isEmpty())
-        {
-            int curr = q.remove();
-            if(vis[curr] == false)
-            {
-                System.out.print(curr+" ");
-                vis[curr] = true;
-                for(int i=0;i<graph[curr].size();i++)
-                {
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
-            }
-        }
-    }
-    
-    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[])
-    {
-
-        System.out.print(curr+" ");
-        vis[curr] = true;
-        for(int i=0;i<graph[curr].size();i++)
-        {
-            Edge e = graph[curr].get(i);
-            if(vis[e.dest] == false)
-            {
-                dfs(graph, e.dest, vis);
-            }
-        }
-    }
     public static void main(String args [])
     {
         int V = 7;
-
         ArrayList<Edge> graph[] = new ArrayList[V];
-        create(graph);
+        createGraph(graph);
+
         for(int i=0;i<graph.length;i++)
         {
-            System.out.println("Vertex "+i+" is connected is :");
+            System.out.println("Vertex "+i+" is coonected to : ");
             for(int j=0;j<graph[i].size();j++)
             {
                 Edge e = graph[i].get(j);
@@ -100,14 +61,5 @@ public class Practice
             }
             System.out.println();
         }
-        boolean vis[] = new boolean[V];
-
-        for(int i=0;i<V;i++)
-        {
-            if(vis[i] == false){
-                dfs(graph, i,vis);
-            }
-        }
-       
     }
 }
