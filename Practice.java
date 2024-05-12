bcc7dff6dcc2e92988cf3c071e14bbf3e88c7817
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-12 21:38:48 +0530

    Changes to be committed: 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Practice
 {
@@ -8,11 +6,13 @@ public class Practice
     {
         int src;
         int dest;
+        int weight;
 
-        Edge(int s,int d)
+        Edge(int s,int d,int w)
         {
             this.src = s;
             this.dest = d;
+            this.weight = w;
         }
     }
 
@@ -20,114 +20,39 @@ public class Practice
     {
         for(int i=0;i<graph.length;i++)
         {
-            graph[i] = new ArrayList<>();
+            graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 2, 2));
 
-        graph[1].add(new Edge(1, 3));
-        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2, 10));
+        graph[1].add(new Edge(1, 3, 0));
+        
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 1, 10));
+        graph[2].add(new Edge(2, 3, -1));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
-
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
-
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
-
-        graph[6].add(new Edge(6, 5));
+        graph[3].add(new Edge(3, 1, 0));
+        graph[3].add(new Edge(3, 2, -1));
     }
 
-    public static void bfs(ArrayList<Edge> graph[],int V)
-    {
-        Queue <Integer> q = new LinkedList<>();
-        boolean vis[] = new boolean[V];
-
-        q.add(0);
-
-        while(!q.isEmpty())
-        {
-            int curr = q.remove();
-            if(vis[curr] == false)
-            {
-                System.out.print(curr+" ");
-                vis[curr] = true;
-                for(int i=0;i<graph[curr].size();i++)
-                {
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
-            }
-        }
-    }
-
-    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[])
-    {
-        System.out.print(curr+ " ");
-        vis[curr] = true;
-
-        for(int i=0;i<graph[curr].size();i++)
-        {
-            Edge e = graph[curr].get(i);
-            if(vis[e.dest] == false)
-            {
-                dfs(graph, e.dest, vis);
-            }
-        }
-    }
-
-    public static void printAllPath(ArrayList<Edge> graph[],int curr,int target,String path,boolean vis[])
-    {
-        if(curr == target)
-        {
-            System.out.println(path);
-            return ;
-        } 
-
-        for(int i=0;i<graph[curr].size();i++)
-        {
-            Edge e = graph[curr].get(i);
-            if(vis[curr] == false)
-            {
-                vis[curr] = true;
-                printAllPath(graph, e.dest, target, path+e.dest, vis);
-                vis[curr] = false;
-            }
-        }
-    }
     public static void main(String args [])
     {
-        int V = 7;
+        int V = 4;
+        @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[V];
-
         createGraph(graph);
-        for(int i=0;i<graph.length;i++)
+
+        // print connected vertices of each vertex
+        for(int i=0; i<graph.length; i++)
         {
-            for(int j=0;j<graph[i].size();j++)
+            System.out.println("Vertex "+i+" is connected to : ");
+            for(int j=0; j<graph[i].size();j++)
             {
                 Edge e = graph[i].get(j);
-                System.out.println("("+e.src+", "+e.dest+")");
+                System.out.print("("+e.src+", "+e.dest+", "+e.weight+")");
             }
             System.out.println();
         }
-        bfs(graph, V);
-        System.out.println();
-
-        boolean vis[] = new boolean[V];
-        dfs(graph, 0, vis);
-        System.out.println();
-
-        System.out.println();
-        int src = 0,target = 5;
-        printAllPath(graph, src, target, "0", new boolean[V]);
     }
 }
