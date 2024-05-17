2175a742da2539abe36df462cdde9541ea770114
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-17 15:29:51 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   Practice.java

--- Practice.java
+++ Practice.java
@@ -1,134 +1,32 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.Scanner;
 public class Practice
 {
-    static class Edge
-    {
-        int src;
-        int dest;
-
-        Edge(int s,int d)
-        {
-            this.src = s;
-            this.dest = d;
-        }
-    }
-    public static void createGraph(ArrayList<Edge> graph[])
-    {
-        for(int i=0;i<graph.length;i++)
-        {
-            graph[i] = new ArrayList<Edge>();
-        }
-
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
-
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
-
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
-
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
-
-        graph[6].add(new Edge(6, 5));
-    }
-
-    public static void bfs(ArrayList<Edge> graph[],int V)
-    {
-        Queue<Integer> q = new LinkedList<>();
-        boolean vis[] = new boolean[V];
-
-        q.add(0);
-        while(!q.isEmpty())
-        {
-            int curr = q.remove();
-
-            if(vis[curr] == false)
-            {
-                System.out.print(curr+" ");
-                vis[curr] = true;
-                for(int i=0;i<graph[curr].size();i++)
-                {
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
-            }
-        }
-    }
-
-    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[])
-    {
-        System.out.print(curr+" ");
-        vis[curr] = true;
-
-        for(int i=0;i<graph[curr].size();i++)
-        {
-            Edge e = graph[curr].get(i);
-            if(vis[e.dest] == false)
-            {
-                dfs(graph, e.dest, vis);
-            }
-        }
-    }
-
-    // public String path = "";
-    public static void printAllPrath(ArrayList<Edge> graph[],int curr,int target,String path,boolean vis[])
-    {
-        if(curr == target)
-        {
-            System.out.println(path);
-            return ;
-        }
-
-        for(int i=0;i<graph[curr].size();i++)
-        {
-            Edge e = graph[curr].get(i);
-            if(!vis[e.dest])
-            {
-                vis[curr] = true;
-                printAllPrath(graph, e.dest, target, path+e.dest, vis);
-                vis[curr] = false;
-            }
-        }
-    }
-
     public static void main(String args [])
     {
-        int V = 7;
-        ArrayList<Edge> graph[] = new ArrayList[V];
-        createGraph(graph);
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size: ");
+        int size = sc.nextInt();
+        int [] n = new int[5];
+        
 
-        for(int i=0;i<graph.length;i++)
+        for(int i=0;i<size;i++)
         {
-            System.out.println("Vertex "+i+" is connected to : ");
-            for(int j=0;j<graph[i].size();j++)
-            {
-                Edge e = graph[i].get(j);
-                System.out.print("("+e.src+", "+e.dest+")");
-            }
-            System.out.println();
+            n[i] = sc.nextInt();
         }
-        bfs(graph, V);
-        System.out.println();
 
-        boolean vis[] = new boolean[V];
-        dfs(graph, 0, vis);
+        System.out.println("Enter element to be search : ");
+        int x = sc.nextInt();
+        for(int i=0;i<size;i++)
+        {
+            if(n[i] == x)
+            {
+                System.out.println(x+" is found at : "+i);
+            }
+            // System.out.print(n[i]+" ");
+            
+        }
+        System.out.println("Element is not present in an array");
 
-        System.out.println();
-        int src = 0,target = 5;
-        printAllPrath(graph, src, target, "0", new boolean[V]);
     }
 }
